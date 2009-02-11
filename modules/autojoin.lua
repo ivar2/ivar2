14b9e00dea92973ffd141d55ed5c588f74c7fc0d
Trond A Ekseth <troeks@gmail.com>
2009-02-11 18:50:46 +0100

    Validate and comment the autojoin module.

--- modules/autojoin.lua
+++ modules/autojoin.lua
@@ -1,8 +1,21 @@
+-- Expects a autojoin table in the config file.
+--
+-- Example:
+-- config = {
+--      ...
+--      autojoin = {
+--           '#chan1', '#chan2',
+--      }
+--      ...
+-- }
+
 return {
 	['^:%S+ 376'] = function(self)
-		for _, chan in next, self.config.autojoin do
-			self:log('INFO', 'Automatically joining %s', chan)
-			self:send('JOIN %s', chan)
+		if(self.config.autojoin) then
+			for _, chan in next, self.config.autojoin do
+				self:log('INFO', 'Automatically joining %s', chan)
+				self:send('JOIN %s', chan)
+			end
 		end
 	end
 }
