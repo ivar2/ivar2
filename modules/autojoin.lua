754b7877aef8c9578c7331d5f68282fa958eb8f3
Trond A Ekseth <troeks@gmail.com>
2009-02-11 18:48:16 +0100

    Simplify the auto-join pattern.

--- modules/autojoin.lua
+++ modules/autojoin.lua
@@ -1,10 +1,8 @@
 return {
-	['^:%S+ (%d+)'] = function(self, num)
-		if(num == '376') then
-			for _, chan in next, self.config.autojoin do
-				self:log('INFO', 'Automatically joining %s', chan)
-				self:send('JOIN %s', chan)
-			end
+	['^:%S+ 376'] = function(self)
+		for _, chan in next, self.config.autojoin do
+			self:log('INFO', 'Automatically joining %s', chan)
+			self:send('JOIN %s', chan)
 		end
 	end
 }
