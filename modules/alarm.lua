7d8dcc0a72a496b7a47dffc1a8d5eea21e77f578
Trond A Ekseth <troeks@gmail.com>
2009-02-14 15:07:13 +0100

    Add msg support to the alarm.

--- modules/alarm.lua
+++ modules/alarm.lua
@@ -1,17 +1,17 @@
 local func = function(self, data)
-	self:privmsg(data.dst, '%s: Timer finished.', data.nick)
+	self:privmsg(data.dst, '%s: %s.', data.nick, data.msg)
 end
 
 return {
-	["^:(%S+) PRIVMSG (%S+) :!alarm (%S+)$"] = function(self, src, dest, msg)
-		if(not msg) then
+	["^:(%S+) PRIVMSG (%S+) :!alarm (%S+)%s?(.*)"] = function(self, src, dest, time, msg)
+		if(not time) then
 			-- usage:
 		else
 			local timer = os.time()
 
-			local hour = msg:match'(%d+)h'
-			local min = msg:match'(%d+)m'
-			local sec = msg:match'(%d+)s'
+			local hour = time:match'(%d+)h'
+			local min = time:match'(%d+)m'
+			local sec = time:match'(%d+)s'
 
 			if(hour) then timer = timer + (hour * 60 * 60) end
 			if(min) then timer = timer + (min * 60) end
@@ -35,6 +35,7 @@ return {
 				table.insert(timers, {
 					nick = nick,
 					dst = dest,
+					msg = (#msg > 0 and msg) or 'Timer finished',
 
 					name = 'Alarm:' .. nick,
 					func = func,
