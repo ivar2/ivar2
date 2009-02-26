2cf052c50ee1927b120ad4eaabcd0d83f8816edb
Trond A Ekseth <troeks@gmail.com>
2009-02-27 00:37:06 +0100

    Make the choose reply for single input random.

--- modules/choose.lua
+++ modules/choose.lua
@@ -5,6 +5,11 @@ local ltrim = function(r, s)
 	return (string.gsub(s, "^" .. r, ""))
 end
 
+local reply = {
+	'┗(＾0＾)┓))(( ┏(＾0＾)┛',
+	'（　´_ゝ`）ﾌｰﾝ',
+}
+
 math.randomseed(os.time())
 
 return {
@@ -24,7 +29,7 @@ return {
 		local seed = math.random(1, #arr)
 
 		if(#arr == 1 or i == 1) then
-			self:msg(dest, src, "（　´_ゝ`）ﾌｰﾝ")
+			self:msg(dest, src, reply[math.random(1, #reply)])
 		else
 			self:msg(dest, src, "%s: %s", src:match"^([^!]+)", ltrim(arr[seed]))
 		end
