062596e5db507e3e522fb3ed7d238a0813f8bdab
Trond A Ekseth <troeks@gmail.com>
2009-03-10 21:56:52 +0100

    Track the exchange module.

---
+++ modules/exchange.lua
@@ -0,0 +1,129 @@
+local cc = {
+	["AED"] = "United Arab Emirates Dirham (AED)",
+	["ANG"] = "Netherlands Antillean Gulden (ANG)",
+	["ARS"] = "Argentine Peso (ARS)",
+
+	["AUD"] = "Australian Dollar (AUD)",
+	["BGN"] = "Bulgarian Lev (BGN)",
+	["BHD"] = "Bahraini Dinar (BHD)",
+	["BND"] = "Brunei Dollar (BND)",
+	["BOB"] = "Bolivian Boliviano (BOB)",
+	["BRL"] = "Brazilian Real (BRL)",
+	["BWP"] = "Botswana Pula (BWP)",
+	["CAD"] = "Canadian Dollar (CAD)",
+	["CHF"] = "Swiss Franc (CHF)",
+
+	["CLP"] = "Chilean Peso (CLP)",
+	["CNY"] = "Chinese Yuan (renminbi) (CNY)",
+	["COP"] = "Colombian Peso (COP)",
+	["CSD"] = "Serbian Dinar (CSD)",
+	["CZK"] = "Czech Koruna (CZK)",
+	["DKK"] = "Danish Krone (DKK)",
+	["EEK"] = "Estonian Kroon (EEK)",
+	["EGP"] = "Egyptian Pound (EGP)",
+	["EUR"] = "Euro (EUR)",
+
+	["FJD"] = "Fijian Dollar (FJD)",
+	["GBP"] = "British Pound (GBP)",
+	["HKD"] = "Hong Kong Dollar (HKD)",
+	["HNL"] = "Honduran Lempira (HNL)",
+	["HRK"] = "Croatian Kuna (HRK)",
+	["HUF"] = "Hungarian Forint (HUF)",
+	["IDR"] = "Indonesian Rupiah (IDR)",
+	["ILS"] = "New Israeli Sheqel (ILS)",
+	["INR"] = "Indian Rupee (INR)",
+
+	["ISK"] = "Icelandic Króna (ISK)",
+	["JPY"] = "Japanese Yen (JPY)",
+	["KRW"] = "South Korean Won (KRW)",
+	["KWD"] = "Kuwaiti Dinar (KWD)",
+	["KZT"] = "Kazakhstani Tenge (KZT)",
+	["LKR"] = "Sri Lankan Rupee (LKR)",
+	["LTL"] = "Lithuanian Litas (LTL)",
+	["MAD"] = "Moroccan Dirham (MAD)",
+	["MUR"] = "Mauritian Rupee (MUR)",
+
+	["MXN"] = "Mexican Peso (MXN)",
+	["MYR"] = "Malaysian Ringgit (MYR)",
+	["NOK"] = "Norwegian Krone (NOK)",
+	["NPR"] = "Nepalese Rupee (NPR)",
+	["NZD"] = "New Zealand Dollar (NZD)",
+	["OMR"] = "Omani Rial (OMR)",
+	["PEN"] = "Peruvian Nuevo Sol (PEN)",
+	["PHP"] = "Philippine Peso (PHP)",
+	["PKR"] = "Pakistani Rupee (PKR)",
+
+	["PLN"] = "Polish Złoty (PLN)",
+	["QAR"] = "Qatari Riyal (QAR)",
+	["RON"] = "New Romanian Leu (RON)",
+	["RUB"] = "Russian Ruble (RUB)",
+	["SAR"] = "Saudi Riyal (SAR)",
+	["SEK"] = "Swedish Krona (SEK)",
+	["SGD"] = "Singapore Dollar (SGD)",
+	["SIT"] = "Slovenian Tolar (SIT)",
+	["SKK"] = "Slovak Koruna (SKK)",
+
+	["THB"] = "Thai Baht (THB)",
+	["TRY"] = "New Turkish Lira (TRY)",
+	["TTD"] = "Trinidad and Tobago Dollar (TTD)",
+	["TWD"] = "New Taiwan Dollar (TWD)",
+	["UAH"] = "Ukrainian Hryvnia (UAH)",
+	["USD"] = "United States Dollar (USD)",
+	["VEB"] = "Venezuelan Bolívar (VEB)",
+	["ZAR"] = "South African Rand (ZAR)",
+}
+
+-- make environment
+local _X = setmetatable({
+	math = math,
+	print = function(val) if(val and tonumber(val)) then return tonumber(val) end end
+}, {__index = math})
+
+-- run code under environment
+local function run(untrusted_code)
+	local untrusted_function, message = loadstring(untrusted_code)
+	if not untrusted_function then return nil, message end
+	setfenv(untrusted_function, _X)
+	return pcall(untrusted_function)
+end
+
+return {
+	["^:(%S+) PRIVMSG (%S+) :!xe (.-) (.-) (%a%a%a)"] = function(self, src, dest, val, from, to)
+		from = from:gsub(' [tToO]+', '')
+		local output
+
+		if(cc[from:upper()] and cc[to:upper()] and from:upper() ~= to:upper()) then
+			local success, val, err = run('return '..val)
+			if(not err) then
+				if(type(val) == 'number' and val > 0 and val ~= math.huge and val ~= (0/0)) then
+					local url = ('http://finance.google.com/finance/converter?a=%s&from=%s&to=%s'):format(val, from, to)
+					local content, status = utils.http(url)
+					if(status == 200) then
+						local data = content:match'<td id="currency_converter_result" align=left>\n(.-)</span>'
+						data = utils.decodeHTML(data:gsub('<.->', '')):gsub('  ', ' ')
+						if(data) then
+							output = data
+						end
+					else
+						output = 'Unable to contact server. Returned status code: '..tostring(status)
+					end
+				else
+					output = 'assertion failed - invalid number data.'
+				end
+			else
+				output = 'assertion failed - '..err
+			end
+		else
+			output = 'Invalid currency.'
+		end
+
+		if dest == self.config.nick then
+			-- Send the response in a PM
+			local srcnick = self:srctonick(src)
+			self:privmsg(srcnick, "%s: %s", srcnick, output)
+		else
+			-- Send it to the channel
+			self:privmsg(dest, "%s: %s", self:srctonick(src), output)
+		end
+	end,
+}
