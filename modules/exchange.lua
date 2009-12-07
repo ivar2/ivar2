00408bd6acbf44e851c40cebea73cd74cdce72b4
Trond A Ekseth <troeks@gmail.com>
2009-12-07 10:48:49 +0100

    Add !cur to exchange.

--- modules/exchange.lua
+++ modules/exchange.lua
@@ -87,43 +87,46 @@ local function run(untrusted_code)
 	return pcall(untrusted_function)
 end
 
-return {
-	["^:(%S+) PRIVMSG (%S+) :!xe (.-) (.-) (%a%a%a)"] = function(self, src, dest, val, from, to)
-		from = from:gsub(' [tToO]+', '')
-		local output
+local exchange = function(self, src, dest, val, from, to)
+	from = from:gsub(' [tToO]+', '')
+	local output
 
-		if(cc[from:upper()] and cc[to:upper()] and from:upper() ~= to:upper()) then
-			local success, val, err = run('return '..val)
-			if(not err) then
-				if(type(val) == 'number' and val > 0 and val ~= math.huge and val ~= (0/0)) then
-					local url = ('http://finance.google.com/finance/converter?a=%s&from=%s&to=%s'):format(val, from, to)
-					local content, status = utils.http(url)
-					if(status == 200) then
-						local data = content:match'<div id=currency_converter_result>(.-)</span>'
-						data = utils.decodeHTML(data:gsub('<.->', '')):gsub('  ', ' ')
-						if(data) then
-							output = data
-						end
-					else
-						output = 'Unable to contact server. Returned status code: '..tostring(status)
+	if(cc[from:upper()] and cc[to:upper()] and from:upper() ~= to:upper()) then
+		local success, val, err = run('return '..val)
+		if(not err) then
+			if(type(val) == 'number' and val > 0 and val ~= math.huge and val ~= (0/0)) then
+				local url = ('http://finance.google.com/finance/converter?a=%s&from=%s&to=%s'):format(val, from, to)
+				local content, status = utils.http(url)
+				if(status == 200) then
+					local data = content:match'<div id=currency_converter_result>(.-)</span>'
+					data = utils.decodeHTML(data:gsub('<.->', '')):gsub('  ', ' ')
+					if(data) then
+						output = data
 					end
 				else
-					output = 'assertion failed - invalid number data.'
+					output = 'Unable to contact server. Returned status code: '..tostring(status)
 				end
 			else
-				output = 'assertion failed - '..err
+				output = 'assertion failed - invalid number data.'
 			end
 		else
-			output = 'Invalid currency.'
+			output = 'assertion failed - '..err
 		end
+	else
+		output = 'Invalid currency.'
+	end
 
-		if dest == self.config.nick then
-			-- Send the response in a PM
-			local srcnick = self:srctonick(src)
-			self:privmsg(srcnick, "%s: %s", srcnick, output)
-		else
-			-- Send it to the channel
-			self:privmsg(dest, "%s: %s", self:srctonick(src), output)
-		end
-	end,
+	if dest == self.config.nick then
+		-- Send the response in a PM
+		local srcnick = self:srctonick(src)
+		self:privmsg(srcnick, "%s: %s", srcnick, output)
+	else
+		-- Send it to the channel
+		self:privmsg(dest, "%s: %s", self:srctonick(src), output)
+	end
+end
+
+return {
+	["^:(%S+) PRIVMSG (%S+) :!xe (.-) (.-) (%a%a%a)"] = exchange,
+	["^:(%S+) PRIVMSG (%S+) :!cur (.-) (.-) (%a%a%a)"] = exchange,
 }
