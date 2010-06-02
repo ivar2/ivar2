89a3b4b43351ba902e513b2cefd076b7706d92f6
Trond A Ekseth <troeks@gmail.com>
2010-06-02 18:35:31 +0200

    No, we can't be that smart.

--- modules/gcalc.lua
+++ modules/gcalc.lua
@@ -4,9 +4,9 @@ return {
 		local content, status = utils.http("http://www.google.com/search?q=" .. msg)
 		if(content) then
 			-- It might explode, but shouldn't!
-			local ans = content:match('<h2 .-><b>(.-)</b></h2><div'):gsub("<[^>]+> ?", "")
+			local ans = content:match('<h2 .-><b>(.-)</b></h2><div')
 			if(ans) then
-				self:msg(dest, src, "%s: %s", src:match"^([^!]+)", ans)
+				self:msg(dest, src, "%s: %s", src:match"^([^!]+)", ans:gsub("<[^>]+> ?", ""))
 			else
 				self:msg(dest, src, '%s: %s', src:match"^([^!]+)", 'Do you want some air with that fail?')
 			end
