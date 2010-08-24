f97bc39804fd544481a17d619f55285313986b0a
Trond A Ekseth <troeks@gmail.com>
2010-08-24 19:24:07 +0200

    Convert HTML entities to readable characters.

--- modules/gcalc.lua
+++ modules/gcalc.lua
@@ -6,7 +6,7 @@ return {
 			-- It might explode, but shouldn't!
 			local ans = content:match('<h2 .-><b>(.-)</b></h2><div')
 			if(ans) then
-				self:msg(dest, src, "%s: %s", src:match"^([^!]+)", ans:gsub("<[^>]+> ?", ""))
+				self:msg(dest, src, "%s: %s", src:match"^([^!]+)", utils.decodeHTML(ans:gsub("<[^>]+> ?", "")))
 			else
 				self:msg(dest, src, '%s: %s', src:match"^([^!]+)", 'Do you want some air with that fail?')
 			end
