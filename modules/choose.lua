df9f3b71a88c75b52db4e756e82b7d28eef4a462
Trond A Ekseth <troeks@gmail.com>
2011-03-12 00:09:36 +0100

    Filter the output.

--- modules/choose.lua
+++ modules/choose.lua
@@ -5,6 +5,37 @@ local ltrim = function(r, s)
 	return (string.gsub(s, "^" .. r, ""))
 end
 
+local patterns = {
+	-- X://Y url
+	"^(%a[%w%.+-]+://%S+)",
+	"%f[%S](%a[%w%.+-]+://%S+)",
+	-- www.X.Y url
+	"^(www%.[%w_-%%]+%.%S+)",
+	"%f[%S](www%.[%w_-%%]+%.%S+)",
+	-- XXX.YYY.ZZZ.WWW:VVVV/UUUUU IPv4 address with port and path
+	"^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d:[0-6]?%d?%d?%d?%d/%S+)",
+	"%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d:[0-6]?%d?%d?%d?%d/%S+)",
+	-- XXX.YYY.ZZZ.WWW/VVVVV IPv4 address with path
+	"^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%/%S+)",
+	"%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%/%S+)",
+	-- XXX.YYY.ZZZ.WWW IPv4 address
+	"^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%)%f[%D]",
+	"%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%)%f[%D]",
+	-- X.Y.Z:WWWW/VVVVV url with port and path
+	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)",
+	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)",
+	-- X.Y.Z:WWWW url with port (ts server for example)
+	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]",
+	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]",
+	-- X.Y.Z/WWWWW url with path
+	--	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)",
+	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)",
+	-- X.Y.Z url
+	--	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+))",
+	--	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+))",
+}
+
+
 local reply = {
 	'┗(＾0＾)┓))(( ┏(＾0＾)┛',
 	'（　´_ゝ`）ﾌｰﾝ',
@@ -25,6 +56,13 @@ return {
 		if(http) then
 			local content, status = utils.http(http)
 			if(content) then
+				-- Strip out any HTML.
+				content = content:gsub('<%/?[%w:]+.-%/?>', '')
+				-- K-Line prevention!
+				for _, pattern in ipairs(patterns) do
+					content = content:gsub(pattern, '<snip />')
+				end
+
 				arr = utils.split(content, '[\n\r]+')
 			else
 				arr = {}
