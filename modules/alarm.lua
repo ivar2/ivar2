95cb0979d594ae79c039c538073f821d1db8d987
Trond A Ekseth <troeks@gmail.com>
2011-06-04 20:52:09 +0200

    Send a notice to the person who adds an alarm.

--- modules/alarm.lua
+++ modules/alarm.lua
@@ -2,6 +2,65 @@ local func = function(self, data)
 	self:msg(data.dst, data.src, '%s: %s', data.nick, data.msg)
 end
 
+local send = function(self, dst, fmt, ...)
+	if(select('#', ...) > 0) then
+		local succ, err = pcall(string.format, fmt, ...)
+		if(not succ) then
+			self:log('ERROR', 'Failed string.format: ' .. tostring(err) .. ' Traceback' .. debug.traceback())
+
+			return
+		end
+
+		fmt = err
+	end
+
+	self:log('INFO', fmt)
+	self:send('NOTICE %s :%s', dst, fmt)
+end
+
+local notice = function(self, dst, src, ...)
+	local srcnick = self:srctonick(src)
+	send(self, srcnick, ...)
+end
+
+local timeTable = {
+	24 * 60 * 60, -- day
+	60 * 60, -- hour
+	60, -- minute
+	1, -- seconds
+}
+
+local timeStrings = {
+	'd', 'd', -- days, day
+	'h', 'h', -- hours, hour
+	'm', 'm', -- minutes, minute
+	's', 's', -- seconds, second
+}
+
+local getRelativeTime = function(t1, t2, T, L)
+	if(not t2) then t2 = os.time() end
+	if(t2 > t1) then t2, t1 = t1, t2 end
+
+	-- Fallbacks
+	T = T or timeTable
+	L = L or timeStrings
+
+	local out
+	local diff = t1 - t2
+	for i=1, #T do
+		local div = T[i]
+		local n = math.modf(diff / div)
+		if(n > 0) then
+			out = string.format(
+			'%s%d%s ',
+			out or '', n, L[(n ~= 1 and i * 2) or (i * 2) - 1])
+			diff = diff % div
+		end
+	end
+
+	return out:sub(1, -2)
+end
+
 local alarm = function(self, src, dest, time, msg)
 	if(not time) then
 		-- usage:
@@ -31,6 +90,8 @@ local alarm = function(self, src, dest, time, msg)
 				timers = {}
 				self.timers = timers
 			end
+
+			notice(self, dest, src, "I'll poke you in %s.", getRelativeTime(timer))
 
 			table.insert(timers, {
 				nick = nick,
