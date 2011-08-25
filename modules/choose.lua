048c6485bf36463a23b5a58410e513a18f6d673c
Trond A Ekseth <troeks@gmail.com>
2011-08-25 21:24:10 +0200

    choose: Port the module over to the new core and remove un-used features.

--- modules/choose.lua
+++ modules/choose.lua
@@ -1,40 +1,11 @@
-local ltrim = function(r, s)
-	if s == nil then
-		s, r = r, "%s+"
-	end
-	return (string.gsub(s, "^" .. r, ""))
+local split = function(str, pattern)
+	local out = {}
+	str:gsub(pattern, function(match)
+		table.insert(out, match)
+	end)
+
+	return out
 end
-
-local patterns = {
-	-- X://Y url
-	"^(%a[%w%.+-]+://%S+)",
-	"%f[%S](%a[%w%.+-]+://%S+)",
-	-- www.X.Y url
-	"^(www%.[%w_-%%]+%.%S+)",
-	"%f[%S](www%.[%w_-%%]+%.%S+)",
-	-- XXX.YYY.ZZZ.WWW:VVVV/UUUUU IPv4 address with port and path
-	"^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d:[0-6]?%d?%d?%d?%d/%S+)",
-	"%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d:[0-6]?%d?%d?%d?%d/%S+)",
-	-- XXX.YYY.ZZZ.WWW/VVVVV IPv4 address with path
-	"^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%/%S+)",
-	"%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%/%S+)",
-	-- XXX.YYY.ZZZ.WWW IPv4 address
-	"^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%)%f[%D]",
-	"%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%)%f[%D]",
-	-- X.Y.Z:WWWW/VVVVV url with port and path
-	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)",
-	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)",
-	-- X.Y.Z:WWWW url with port (ts server for example)
-	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]",
-	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]",
-	-- X.Y.Z/WWWWW url with path
-	--	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)",
-	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)",
-	-- X.Y.Z url
-	--	"^([%w_-%%%.]+[%w_-%%]%.(%a%a+))",
-	--	"%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+))",
-}
-
 
 local reply = {
 	'┗(＾0＾)┓))(( ┏(＾0＾)┛',
@@ -49,43 +20,26 @@ local reply = {
 math.randomseed(os.time() % 1e5)
 
 return {
-	["^:(%S+) PRIVMSG (%S+) :!choose (.+)$"] = function(self, src, dest, msg)
-		local hax = {}
-		local http = msg:match("^%s*(http://%S*)")
-		local arr
-		if(http) then
-			local content, status = utils.http(http)
-			if(content) then
-				-- Strip out any HTML.
-				content = content:gsub('<%/?[%w:]+.-%/?>', '')
-				-- K-Line prevention!
-				for _, pattern in ipairs(patterns) do
-					content = content:gsub(pattern, '<snip />')
-				end
+	PRIVMSG = {
+		['!choose (.*)$'] = function(self, source, destination, choices)
+			local hax = {}
+			local arr = split(choices, '%s*([^,]+)%s*')
 
-				arr = utils.split(content, '[\n\r]+')
-			else
-				arr = {}
+			for k, v in pairs(arr) do
+				hax[v] = true
 			end
-		else
-			arr = utils.split(msg, ",[%s]?")
-		end
 
-		for k, v in pairs(arr) do
-			hax[v] = true
-		end
+			local i = 0
+			for k, v in pairs(hax) do
+				i = i + 1
+			end
 
-		local i = 0
-		for k, v in pairs(hax) do
-			i = i + 1
+			if(#arr == 1 or i == 1) then
+				self:Msg('privmsg', destination, source, reply[math.random(1, #reply)])
+			else
+				local seed = math.random(1, #arr)
+				self:Msg('privmsg', destination, source, '%s: %s', source.nick, arr[seed])
+			end
 		end
-
-		local seed = math.random(1, #arr)
-
-		if(#arr == 1 or i == 1) then
-			self:msg(dest, src, reply[math.random(1, #reply)])
-		else
-			self:msg(dest, src, "%s: %s", src:match"^([^!]+)", ltrim(arr[seed]))
-		end
-	end
+	}
 }
