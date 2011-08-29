7510abf7d42301747ba8e118a9afd9643e59ee90
Trond A Ekseth <troeks@gmail.com>
2011-08-29 19:30:25 +0200

    gcalc: Port gcalc over to the new core.

--- modules/gcalc.lua
+++ modules/gcalc.lua
@@ -1,19 +1,63 @@
-local handle = function(self, src, dest, msg)
-	msg = utils.escape(msg):gsub('%s', '+')
-	local content, status = utils.http("http://www.google.com/search?q=" .. msg)
-	if(content) then
-		-- It might explode, but shouldn't!
-		local ans = content:match('<h2 .-><b>(.-)</b></h2><div')
-		if(ans) then
-			self:msg(dest, src, "%s: %s", src:match"^([^!]+)", utils.decodeHTML(ans:gsub("<sup>(.-)</sup>", "^%1"):gsub("<[^>]+> ?", "")))
-		else
-			self:msg(dest, src, '%s: %s', src:match"^([^!]+)", 'Do you want some air with that fail?')
+package.path = table.concat(
+	{
+		'libs/?.lua',
+		'libs/?/init.lua',
+
+		'',
+	}, ';'
+) .. package.path
+
+local httpclient = require'handler.http.client'
+local html2unicode = require'html'
+
+local ivar2 = ...
+local client = httpclient.new(ivar2.Loop)
+
+local urlEncode = function(str)
+	return str:gsub(
+		'([^%w ])',
+		function (c)
+			return string.format ("%%%02X", string.byte(c))
 		end
+	):gsub(' ', '+')
+end
+
+local parseData = function(self, source, destination, data)
+	local ans = data:match('<h2 .-><b>(.-)</b></h2><div')
+	if(ans) then
+		ans = ans:gsub('<sup>(.-)</sup>', '^%1'):gsub('<[^>]+> ?', '')
+		self:Msg('privmsg', destination, source, '%s: %s', source.nick, html2unicode(ans))
+	else
+		self:Msg('privmsg', destination, source, '%s: %s', source.nick, 'Do you want some air with that fail?')
 	end
 end
 
+local handle = function(self, source, destination, input)
+	local search = urlEncode(input)
+
+	local sink = {}
+	client:request{
+		host ='www.google.com',
+		port = 80,
+		scheme = 'http',
+		method = 'GET',
+		path = ('/search?q=%s'):format(search),
+		stream_response = true,
+
+		on_data = function(request, response, data)
+			if(data) then sink[#sink + 1] = data end
+		end,
+
+		on_finished = function()
+			parseData(self, source, destination, table.concat(sink))
+		end,
+	}
+end
+
 return {
-	["^:(%S+) PRIVMSG (%S+) :!gcalc (.+)$"] = handle,
-	["^:(%S+) PRIVMSG (%S+) :!calc (.+)$"] = handle,
-	["^:(%S+) PRIVMSG (%S+) :!galc (.+)$"] = handle,
+	PRIVMSG = {
+		['!gcalc (.+)$'] = handle,
+		['!calc (.+)$'] = handle,
+		['!galc (.+)$'] = handle,
+	},
 }
