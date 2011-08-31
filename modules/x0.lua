899231e3b23eaefd916e9836af3430ff635fefb8
Trond A Ekseth <troeks@gmail.com>
2011-08-31 22:00:30 +0200

    x0: Delete the old file.

--- modules/x0.lua
+++
@@ -1,9 +0,0 @@
-local handler = function(self, src, dest, msg)
-	local short = utils.x0(msg)
-	if(short) then self:msg(dest, src, "%s: %s", self:srctonick(src), short) end
-end
-
-return {
-	["^:(%S+) PRIVMSG (%S+) :!shorten (.+)$"] = handler,
-	["^:(%S+) PRIVMSG (%S+) :!x0 (.+)$"] = handler,
-}
