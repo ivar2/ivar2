d7e3beb43c24d215f0008f664f1453572370bb0c
Trond A Ekseth <troeks@gmail.com>
2011-08-31 22:00:07 +0200

    shorten: Rename x0 to shorten and port it over to the new core.

---
+++ modules/shorten.lua
@@ -0,0 +1,15 @@
+local x0 = require'x0'
+x0.init(ivar2.Loop)
+
+local handler = function(self, source, destination, input)
+	x0.lookup(input, function(url)
+		self:Msg('privmsg', destination, source, '%s: %s', source.nick, url)
+	end)
+end
+
+return {
+	PRIVMSG = {
+		['!shorten (.+)$'] = handler,
+		['!x0 (.+)$'] = handler,
+	},
+}
