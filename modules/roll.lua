aeb1388081155e44091da851141edfebf71c3aa3
Trond A Ekseth <troeks@gmail.com>
2011-09-02 22:24:39 +0200

    roll: Ported to the new core.

--- modules/roll.lua
+++ modules/roll.lua
@@ -1,27 +1,26 @@
 local ltrim = function(r, s)
-	if s == nil then
+	if(not s) then
 		s, r = r, "%s+"
 	end
 	return (string.gsub(s, "^" .. r, ""))
 end
 
-math.randomseed(os.time() % 1e5)
-
 return {
-	["^:(%S+) PRIVMSG (%S+) :!roll (.+)$"] = function(self, src, dest, msg)
-		local a,b = unpack(utils.split(msg, "%s+"))
-		a, b = tonumber(a), tonumber(b)
+	PRIVMSG = {
+		['!roll (.+)$'] = function(self, source, destination, input)
+			local a,b = input:match('([-%d]+)%s*([-%d]*)')
+			a, b = tonumber(a), tonumber(b)
 
-		local nick = self:srctonick(src)
-		local seed
-		if(a and b and a < b) then
-			seed = math.random(a, b)
-		elseif(a and a > 1) then
-			seed = math.random(1, a)
-		else
-			return self:msg(dest, src, '%s: %s', nick, 'I will suffocate you with a pillow in your sleep')
-		end
+			local seed
+			if(a and b and a < b) then
+				seed = math.random(a, b)
+			elseif(a and a > 1) then
+				seed = math.random(1, a)
+			else
+				seed = 'I will suffocate you with a pillow in your sleep!'
+			end
 
-		self:msg(dest, src, "%s: %s", nick, seed)
-	end
+			self:Msg('privmsg', destination, source, '%s: %s', source.nick, seed)
+		end,
+	},
 }
