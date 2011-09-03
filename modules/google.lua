40c4f7094761bd224fd913cf77f794df4f5acdc5
Trond A Ekseth <troeks@gmail.com>
2011-09-03 16:53:10 +0200

    google: Port the module over to the new core.

--- modules/google.lua
+++ modules/google.lua
@@ -9,9 +9,13 @@
 --     ...
 -- }
 
-require'json'
+local x0 = require'x0'
+local httpclient = require'handler.http.client'
+local json = require'json'
+local html2unicode = require'html'
 
-local url = 'http://www.google.com/uds/GwebSearch?context=0&hl=en&key=%s&v=1.0&q=%s&rsz=small'
+local client = httpclient.new(ivar2.Loop)
+x0.init(ivar2.Loop)
 
 local utify8 = function(str)
 	str = str:gsub("\\u(....)", function(n)
@@ -29,41 +33,73 @@ local utify8 = function(str)
 	return str
 end
 
-local handler = function(self, src, dest, msg)
-	if(self.config.googleAPIKey) then
-		msg = utils.escape(msg):gsub('%s', '+')
+local urlEncode = function(str)
+	return str:gsub(
+		'([^%w ])',
+		function (c)
+			return string.format ("%%%02X", string.byte(c))
+		end
+	):gsub(' ', '+')
+end
 
-		local content, status = utils.http(url:format(self.config.googleAPIKey, msg))
-		if(status == 200) then
-			local data = utify8(content)
-			data = json.decode(data)
+local outFormat = '\002%s\002 <%s>'
+local parseData = function(self, source, destination, data)
+	data = utify8(data)
+	data = json.decode(data)
 
-			if(data and data.responseStatus == 200) then
-				local arr = {}
-				for i=1,3 do
-					local match = data.responseData.results[i]
-					if(not match) then break end
+	if(data and data.responseStatus == 200) then
+		local arr = {}
+		local n = 0
+		for i=1,3 do
+			local match = data.responseData.results[i]
+			if(not match) then break end
 
-					local title = utils.decodeHTML(match.titleNoFormatting)
-					local url = match.unescapedUrl
-					if(#url >= 75) then
-						url = utils.x0(url) or url
+			local title = html2unicode(match.titleNoFormatting)
+			local url = match.unescapedUrl
+			if(#url >= 75) then
+				n = n + 1
+				x0.lookup(url, function(short)
+					n = n - 1
+					arr[i] = outFormat:format(title, short or url)
+
+					if(n == 0) then
+						self:Msg('privmsg', destination, source, table.concat(arr, ' || '))
 					end
-					table.insert(arr, ('\002%s\002 <%s>'):format(title, url))
-				end
-
-				if(#arr ~= 0) then
-					return self:msg(dest, src, table.concat(arr, " || "))
-				end
-
-				if(#arr == 0) then
-					self:msg(dest, src, "jack shit found...")
-				end
+				end)
+			else
+				arr[i] = outFormat:format(title, url)
 			end
+		end
+
+		if(#arr == 0 and n == 0) then
+			self:Msg('privmsg', destination, source, 'jack shit found :-(.')
+		elseif(#arr == 3) then
+			self:Msg('privmsg', destination, source, table.concat(arr, ' || '))
 		end
 	end
 end
 
+local url = 'http://www.google.com/uds/GwebSearch?context=0&hl=en&key=%s&v=1.0&q=%s&rsz=small'
+local handler = function(self, source, destination, input)
+	local search = urlEncode(input)
+
+	local sink = {}
+	client:request{
+		url = url:format(self.config.googleAPIKey, search),
+
+		on_data = function(request, response, data)
+			if(data) then sink[#sink + 1] = data end
+		end,
+
+		on_finished = function()
+			parseData(self, source, destination, table.concat(sink))
+		end,
+	}
+end
+
 return {
-	["^:(%S+) PRIVMSG (%S+) :!g (.+)$"] = handler,
+	PRIVMSG = {
+		['!g (.+)$'] = handler,
+		['!google (.+)$'] = handler,
+	},
 }
