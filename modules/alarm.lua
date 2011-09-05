fa948a9883003e9b13ef8fc0c7a542d1e016ca40
Trond A Ekseth <troeks@gmail.com>
2011-09-05 21:23:35 +0200

    alarm: Port the module over to the new core and clean stuff up.

--- modules/alarm.lua
+++ modules/alarm.lua
@@ -1,121 +1,49 @@
-local func = function(self, data)
-	self:msg(data.dst, data.src, '%s: %s', data.nick, data.msg)
-end
+local ev = require'ev'
+local date = require'date'
 
-local send = function(self, dst, fmt, ...)
-	if(select('#', ...) > 0) then
-		local succ, err = pcall(string.format, fmt, ...)
-		if(not succ) then
-			self:log('ERROR', 'Failed string.format: ' .. tostring(err) .. ' Traceback' .. debug.traceback())
+if(not ivar2.timers) then ivar2.timers = {} end
 
-			return
-		end
+local alarm = function(self, source, destination, time, message)
+	local hour = time:match'(%d+)[ht]'
+	local min = time:match'(%d+)m'
+	local sec = time:match'(%d+)s'
 
-		fmt = err
+	local duration = 0
+	if(hour) then duration = duration + (hour * 60 * 60) end
+	if(min) then duration = duration + (min * 60) end
+	if(sec) then duration = duration + sec end
+
+	-- 60 days or more?
+	local nick = source.nick
+	if(duration >= (60 * 60 * 24 * 60) or duration == 0) then
+		return self:Msg(dest, src, "%s: :'(", nick)
 	end
 
-	self:log('INFO', fmt)
-	self:send('NOTICE %s :%s', dst, fmt)
-end
-
-local notice = function(self, dst, src, ...)
-	local srcnick = self:srctonick(src)
-	send(self, srcnick, ...)
-end
-
-local timeTable = {
-	24 * 60 * 60, -- day
-	60 * 60, -- hour
-	60, -- minute
-	1, -- seconds
-}
-
-local timeStrings = {
-	'd', 'd', -- days, day
-	'h', 'h', -- hours, hour
-	'm', 'm', -- minutes, minute
-	's', 's', -- seconds, second
-}
-
-local getRelativeTime = function(t1, t2, T, L)
-	if(not t2) then t2 = os.time() end
-	if(t2 > t1) then t2, t1 = t1, t2 end
-
-	-- Fallbacks
-	T = T or timeTable
-	L = L or timeStrings
-
-	local out
-	local diff = t1 - t2
-	for i=1, #T do
-		local div = T[i]
-		local n = math.modf(diff / div)
-		if(n > 0) then
-			out = string.format(
-			'%s%d%s ',
-			out or '', n, L[(n ~= 1 and i * 2) or (i * 2) - 1])
-			diff = diff % div
-		end
+	local id = 'Alarm: ' .. nick
+	if(self.timers[id]) then
+		-- message is probably changed.
+		self.timers[id]:stop(ivar2.Loop)
 	end
 
-	return out:sub(1, -2)
-end
+	local timer = ev.Timer.new(
+		function(loop, timer, revents)
+			if(#message == 0) then message = 'Timer finished.' end
+			self:Msg('privmsg', destination, source, '%s: %s', nick, message or 'Timer finished.')
+		end,
+		duration
+	)
 
-local alarm = function(self, src, dest, time, msg)
-	if(not time) then
-		-- usage:
-	else
-		local timer = os.time()
+	-- I implemented getRelativeTimeShort and was like... FFFUUUuu this is just
+	-- the input all over again!
+	self:Notice(source.nick, "I'll poke you in %s.", date.relativeTimeShort(os.time() + duration))
 
-		local hour = time:match'(%d+)[ht]'
-		local min = time:match'(%d+)m'
-		local sec = time:match'(%d+)s'
-
-		local duration = 0
-		if(hour) then duration = duration + (hour * 60 * 60) end
-		if(min) then duration = duration + (min * 60) end
-		if(sec) then duration = duration + sec end
-
-		-- 60 days or more?
-		local nick = self:srctonick(src)
-		if(duration >= (60 * 60 * 24 * 60)) then
-			return self:msg(dest, src, "%s: :'(", nick)
-		end
-
-		timer = timer + duration
-		if(timer ~= os.time()) then
-			local timers = self.timers
-			local id = 'Alarm:' .. nick
-			if(timers) then
-				for index, timerData in pairs(timers) do
-					if(timerData.name == id) then
-						table.remove(timers, index)
-						break;
-					end
-				end
-			else
-				timers = {}
-				self.timers = timers
-			end
-
-			notice(self, dest, src, "I'll poke you in %s.", getRelativeTime(timer))
-
-			table.insert(timers, {
-				nick = nick,
-				dst = dest,
-				src = src,
-				msg = (#msg > 0 and msg) or 'Timer finished.',
-
-				name = id,
-				func = func,
-				callTime = timer,
-				oneCall = true,
-			})
-		end
-	end
+	self.timers[id] = timer
+	timer:start(ivar2.Loop)
 end
 
 return {
-	["^:(%S+) PRIVMSG (%S+) :!alarm (%S+)%s?(.*)"] = alarm,
-	["^:(%S+) PRIVMSG (%S+) :!timer (%S+)%s?(.*)"] = alarm,
+	PRIVMSG = {
+		['^!alarm (%S+)%s?(.*)'] = alarm,
+		['^!timer (%S+)%s?(.*)'] = alarm,
+	},
 }
