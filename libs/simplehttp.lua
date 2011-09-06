df3f81b65f38e828331285fe3fb58490ac3efac8
Trond A Ekseth <troeks@gmail.com>
2011-09-06 19:15:40 +0200

    simplehttp: Support redirects.

--- libs/simplehttp.lua
+++ libs/simplehttp.lua
@@ -2,9 +2,15 @@ local httpclient = require'handler.http.client'
 local ev = require'ev'
 local client = httpclient.new(ev.Loop.default)
 
-return function(url, callback, stream, limit)
+local function simplehttp(url, callback, stream, limit, visited)
 	local sinkSize = 0
 	local sink = {}
+	local visited = visited or {}
+
+	local resp
+	-- Prevent infinite loops!
+	if(visited[url]) then return end
+	visited[url] = true
 
 	client:request{
 		url = url,
@@ -12,6 +18,7 @@ return function(url, callback, stream, limit)
 
 		on_data = function(request, response, data)
 			if(data) then
+				resp = response
 				sinkSize = sinkSize + #data
 				sink[#sink + 1] = data
 				if(limit and sinkSize > limit) then
@@ -21,8 +28,14 @@ return function(url, callback, stream, limit)
 			end
 		end,
 
-		on_finished = function(response)
-			callback(table.concat(sink), url, response)
+		on_finished = function()
+			if(resp.status_code == 301) then
+				return simplehttp(resp.headers.Location, callback, stream, limit, visited)
+			end
+
+			callback(table.concat(sink), url, resp)
 		end,
 	}
 end
+
+return simplehttp
