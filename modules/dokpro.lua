bc6542d81929914b87781596d130646b8e87387f
Trond A Ekseth <troeks@gmail.com>
2011-09-10 15:13:28 +0200

    dokpro: Port over to the new core.
    Also rewrite the parser to suck less and be more general.

--- modules/dokpro.lua
+++ modules/dokpro.lua
@@ -1,31 +1,99 @@
+local simplehttp = require'simplehttp'
 local iconv = require"iconv"
 local iso2utf = iconv.new("utf-8", "iso-8859-15")
 local utf2iso = iconv.new('iso-8859-15', 'utf-8')
 
-return {
-	["^:(%S+) PRIVMSG (%S+) :!dokpro (.+)$"] = function(self, src, dest, msg)
-		local query = utils.escape(utf2iso:iconv(msg)):gsub('%s', '+')
-		print(query)
-		local content, status = utils.http("http://www.nob-ordbok.uio.no/perl/ordbok.cgi?ordbok=bokmaal&bokmaal=+&OPP=" .. query)
+local urlEncode = function(str)
+	return str:gsub(
+		'([^%w ])',
+		function (c)
+			return string.format ("%%%02X", string.byte(c))
+		end
+	):gsub(' ', '+')
+end
 
-		if(content) then
-			-- It might explode, but shouldn't!
-			local ans = content:match('</table></td><td>(.-)</table>')
-			local nick = src:match'^([^!]+)'
-			if(ans) then
-				ans = iso2utf:iconv(ans)
-				-- strip out all the html and convert entities:
-				ans = utils.decodeHTML(ans:gsub('<%/?[%w:]+.-%/?>', ''))
-				self:msg(
-					dest, src,
-					"%s: %s | http://x0.no/dokpro/%s",
-					nick,
-					ans,
-					msg:gsub('%s', '+')
-				)
-			else
-				self:msg(dest, src, '%s: %s', nick, 'Du suger, prøv igjen')
+local trim = function(s)
+	return s:match('^()%s*$') and '' or s:match('^%s*(.*%S)')
+end
+
+local parseData = function(data)
+	data = iso2utf:iconv(data)
+
+	-- This page is a typical example of someone using XHTML+CSS+JS, while still
+	-- coding like they used to back in 1998.
+	data = data:gsub('\r', ''):match('<div id="kolonne_enkel"[^>]+>(.-)</div>'):gsub('&nbsp;', '')
+
+	local words = {}
+	local entryIndex = 1
+	for entryData in data:gmatch('<table class="liten_ordliste">([^\n]+)') do
+		local entry = {
+			lookup = {},
+			examples = {},
+		}
+
+		local insertAt
+		for td in entryData:gmatch('<td[^>]->(.-)</td>') do
+			-- Strip away HTML.
+			local line = trim(td:gsub('<span class="b">([^%d]-)</span>', '%1'):gsub('</?[%w:]+[^>]-/?>', ''))
+			line = line:gsub('%s%s+', ' ')
+
+			if(#line > 0) then
+				if(tonumber(line)) then
+					insertAt = tonumber(line)
+				elseif(not line:match('%s+')) then
+					table.insert(entry.lookup, line)
+				elseif(insertAt) then
+					entry.examples[insertAt] = line
+					insertAt = nil
+				else
+					entry.meaning = line
+				end
 			end
 		end
+
+		table.insert(words, entry)
 	end
+
+	return words
+end
+
+local handleInput = function(self, source, destination, word)
+	local query = urlEncode(utf2iso:iconv(word))
+	simplehttp(
+		"http://www.nob-ordbok.uio.no/perl/ordbok.cgi?ordbok=bokmaal&bokmaal=+&OPP=" .. query,
+
+		function(data)
+			local words = parseData(data)
+
+			local msgLimit = (512 - 16 - 65 - 10) - #self.config.nick - #destination
+			-- size of the word + x0 url.
+			local n =  #word + 23
+			local out = {}
+			for i=1, #words do
+				local word = words[i]
+				local lookup = table.concat(word.lookup, ', ')
+				local message = string.format('\002[%s]\002: %s', lookup, word.meaning)
+
+				n = n + #message
+				if(n < msgLimit) then
+					table.insert(out, message)
+				else
+					break
+				end
+			end
+
+			if(#out > 0) then
+				self:Msg('privmsg', destination, source, '%s | http://x0.no/dokpro/%s', table.concat(out, ', '), word)
+			else
+				self:Msg('privmsg', destination, source, '%s: %s', source.nick, 'Du suger, prøv igjen.')
+			end
+		end
+	)
+end
+
+return {
+	PRIVMSG = {
+		['^!dokpro (.+)$'] = handleInput,
+		['^!ordbok (.+)$'] = handleInput,
+	},
 }
