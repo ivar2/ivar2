4ba9aa54f83641c8b88dc0e822796ce7c65e8e4f
Trond A Ekseth <troeks@gmail.com>
2011-10-02 14:07:49 +0200

    x0: Save the x0 data to cache/ instead of data/.

--- libs/x0.lua
+++ libs/x0.lua
@@ -5,7 +5,7 @@ local x0 = tokyocabinet.hdbnew()
 
 return {
 	lookup = function(url, callback)
-		x0:open('data/x0', x0.OWRITER + x0.OCREAT)
+		x0:open('cache/x0', x0.OWRITER + x0.OCREAT)
 		if(x0[url]) then
 			local url = x0[url]
 			x0:close()
@@ -20,7 +20,7 @@ return {
 			'http://api.x0.no/?' .. url,
 			function(data, url)
 				if(data:sub(8,9) =='x0') then
-					x0:open('data/x0', x0.OWRITER + x0.OCREAT)
+					x0:open('cache/x0', x0.OWRITER + x0.OCREAT)
 					x0[url] = data
 					x0:close()
 
