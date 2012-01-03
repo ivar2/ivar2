06da423a6b1c4e77c355a501d6d69dad44f611cd
Trond A Ekseth <troeks@gmail.com>
2012-01-03 19:53:34 +0100

    lastfm: Convert unicode escapes into characters.

--- modules/lastfm.lua
+++ modules/lastfm.lua
@@ -1,6 +1,22 @@
 local simplehttp = require'simplehttp'
 local json = require'json'
 
+local utify8 = function(str)
+	str = str:gsub("\\u(....)", function(n)
+		n = tonumber(n, 16)
+
+		if(n < 128) then
+			return string.char(n)
+		elseif(n < 2048) then
+			return string.char(192 + ((n - (n % 64)) / 64), 128 + (n % 64))
+		else
+			return string.char(224 + ((n - (n % 4096)) / 4096), 128 + (((n % 4096) - (n % 64)) / 64), 128 + (n % 64))
+		end
+	end)
+
+	return str
+end
+
 local APIBase = 'http://ws.audioscrobbler.com/2.0/?format=json&api_key=' .. ivar2.config.lastfmAPIKey
 
 local buildQuery = function(param)
@@ -15,7 +31,7 @@ end
 
 local pattern = ('<td>[^<]+</td><td>([^<]+)</td>'):rep(3) .. '<td>([^<]+)</td>'
 local parseData = function(source, destination, data)
-	local response = json.decode(data)
+	local response = json.decode(utify8(data))
 	if(response.error) then
 		return ivar2:Msg('privmsg', destination, source, response.message)
 	end
