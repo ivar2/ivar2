e8bc9600eef3201949f49f533112feb264650ff3
Trond A Ekseth <troeks@gmail.com>
2012-06-17 19:32:28 +0200

    title: Support youtu.be as well.

--- modules/title/sites/youtube.lua
+++ modules/title/sites/youtube.lua
@@ -1,6 +1,42 @@
 local simplehttp = require'simplehttp'
 local html2unicode = require'html'
 
+local fetchInformation = function(queue, vid)
+	simplehttp(
+		'https://gdata.youtube.com/feeds/api/videos/' .. vid,
+
+		function(data)
+			local title = html2unicode(data:match("<title type='text'>([^<]+)</title>"))
+			local uploader = html2unicode(data:match('<author><name>([^<]+)</name>'))
+			local duration = tonumber(data:match("<yt:duration seconds='(%d+)'/>"))
+
+			local output
+			if(duration) then
+				if(duration > 3600) then
+					duration = string.format(
+						'%d:%02d:%02d',
+						math.floor(duration / 3600),
+						math.floor((duration % 3600) / 60),
+						duration % 60
+					)
+				else
+					duration = string.format(
+						'%d:%02d',
+						math.floor(duration / 60),
+						duration % 60
+					)
+				end
+
+				output = string.format('%s (%s) by %s', title, duration, uploader)
+			else
+				output = string.format('%s by %s', title, uploader)
+			end
+
+			queue:done(output)
+		end
+	)
+end
+
 customHosts['youtube%.com'] = function(queue, info)
 	local query = info.query
 	local path = info.path
@@ -22,39 +58,22 @@ customHosts['youtube%.com'] = function(queue, info)
 	end
 
 	if(vid) then
-		simplehttp(
-			'https://gdata.youtube.com/feeds/api/videos/' .. vid,
+		fetchInformation(queue, vid)
 
-			function(data)
-				local title = html2unicode(data:match("<title type='text'>([^<]+)</title>"))
-				local uploader = html2unicode(data:match('<author><name>([^<]+)</name>'))
-				local duration = tonumber(data:match("<yt:duration seconds='(%d+)'/>"))
+		return true
+	end
+end
 
-				local output
-				if(duration) then
-					if(duration > 3600) then
-						duration = string.format(
-							'%d:%02d:%02d',
-							math.floor(duration / 3600),
-							math.floor((duration % 3600) / 60),
-							duration % 60
-						)
-					else
-						duration = string.format(
-							'%d:%02d',
-							math.floor(duration / 60),
-							duration % 60
-						)
-					end
+customHosts['youtu.be'] = function(queue, info)
+	local path = info.path
+	local vid
 
-					output = string.format('%s (%s) by %s', title, duration, uploader)
-				else
-					output = string.format('%s by %s', title, uploader)
-				end
+	if(path and path:match('/([a-zA-Z0-9_-]+)')) then
+		vid = path:match('/([a-zA-Z0-9_-]+)')
+	end
 
-				queue:done(output)
-			end
-		)
+	if(vid) then
+		fetchInformation(queue, vid)
 
 		return true
 	end
