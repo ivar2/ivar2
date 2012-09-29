09efac67b95ae5e1176f8a6c7c3bc30c3d952b91
Trond A Ekseth <troeks@gmail.com>
2012-09-29 20:28:16 +0200

    title: Don't blow up on IDN domains.

--- modules/title/init.lua
+++ modules/title/init.lua
@@ -31,12 +31,20 @@ end
 
 -- RFC 2396, section 1.6, 2.2, 2.3 and 2.4.1.
 local smartEscape = function(str)
+	local pathOffset = str:match("//[^/]+/()")
+
+	-- No path means nothing to escape.
+	if(not pathOffset) then return str end
+	local prePath = str:sub(1, pathOffset - 1)
+
 	-- lowalpha: a-z | upalpha: A-Z | digit: 0-9 | mark: -_.!~*'() |
 	-- reserved: ;/?:@&=+$, | delims: <>#%" | unwise: {}|\^[]` | space: <20>
 	local pattern = '[^a-zA-Z0-9%-_%.!~%*\'%(%);/%?:@&=%+%$,<>#%%"{}|\\%^%[%] ]'
-	return str:gsub(pattern, function(c)
+	local path = str:sub(pathOffset):gsub(pattern, function(c)
 		return ('%%%02X'):format(c:byte())
 	end)
+
+	return prePath .. path
 end
 
 local parseAJAX
