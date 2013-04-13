f3566af4b43d51d3576d7b7f2603adcf7e0b3f53
Trond A Ekseth <troeks@gmail.com>
2013-04-14 00:38:21 +0200

    urbandict: Change how we check for valid output.

--- modules/urbandict.lua
+++ modules/urbandict.lua
@@ -13,7 +13,7 @@ end
 local parseJSON = function(data)
 	data = json.decode(data)
 
-	if(data.has_related_words) then
+	if(#data.list > 0) then
 		return data.list[1].definition:gsub('\r\n', ' ')
 	end
 end
