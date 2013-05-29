eac03bebba2c638e070cdef11961dd4180c88bf1
Trond A Ekseth <troeks@gmail.com>
2013-05-30 00:02:59 +0200

    admin: Initial import.

---
+++ modules/admin.lua
@@ -0,0 +1,57 @@
+local verifyOwner = function(src)
+	for _, mask in next, ivar2.config.owners do
+		if(mask == src.mask) then
+			return true
+		end
+	end
+end
+
+return {
+	PRIVMSG = {
+		['madd>%s*(%S+)$'] = function(self, source, destination, module)
+			if(not verifyOwner(source)) then return end
+
+			self:LoadModule(module)
+
+			self:Msg('privmsg', destination, source, "Loaded module: %s", module)
+		end,
+
+		['mdel>%s*(%S+)$'] = function(self, source, destination, module)
+			if(not verifyOwner(source)) then return end
+
+			self:DisableModule(module)
+
+			self:Msg('privmsg', destination, source, "Disable module: %s", module)
+		end,
+
+		['irc> (%S+) (.+)$"'] = function(self, source, destination, command, argument)
+			if(not verifyOwner(source)) then return end
+
+			command = command:lower()
+			if(command == "join") then
+				local chan, pass = argument:match('^(%S+) ?(%S*)')
+				if(pass == '') then pass = nil end
+				self:Join(chan, pass)
+			elseif(command == "part") then
+				self:Part(argument)
+			elseif(command == "nick") then
+				self:Nick(argument)
+			elseif(command == "mode") then
+				local destination, mode = argument:match('^(%S+) (.+)$')
+				self:Mode(destination, mode)
+			elseif(command == "topic") then
+				local chan, topic = argument:match('^(%S+) (.+)$')
+				self:Topic(chan, topic)
+			elseif(command == "kick") then
+				local chan, user, comment = argument:match('^(%S+) (%S+) ?(.*)$')
+				self:Kick(chan, user, comment)
+			end
+		end,
+
+		['reload>'] = function(self, source, destination)
+			if(not verifyOwner(source)) then return end
+
+			self:Reload()
+		end
+	},
+}
