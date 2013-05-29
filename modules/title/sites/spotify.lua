81fa6ae81c13c6253fac911524a6ba15e43802aa
Trond A Ekseth <troeks@gmail.com>
2013-05-29 17:55:09 +0200

    title: Handle play.spotify.com URLs as well.

--- modules/title/sites/spotify.lua
+++ modules/title/sites/spotify.lua
@@ -19,3 +19,22 @@ customHosts['open.spotify.com'] = function(queue, info)
 		return true
 	end
 end
+
+customHosts['play.spotify.com'] = function(queue, info)
+	local path = info.path
+
+	if(path and path:match'/(%w+)/(.+)') then
+		simplehttp(
+			info.url:gsub("play%.spotify", "open.spotify"),
+
+			function(data, url, response)
+				local title = html2unicode(data:match'<title>(.-) on Spotify</title>')
+				local uri = data:match('property="og:audio" content="([^"]+)"')
+
+				queue:done(string.format('%s: %s | http://open.spotify.com/%s', title, uri, info.path))
+			end
+		)
+
+		return true
+	end
+end
