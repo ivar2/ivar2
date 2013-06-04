f0e4dfada87e1057b3edb3738db0fced6067b791
Trond A Ekseth <troeks@gmail.com>
2013-06-04 23:32:19 +0200

    event: Initial import.

---
+++ libs/event.lua
@@ -0,0 +1,44 @@
+local argcheck = function(value, num, ...)
+	assert(type(num) == 'number', "Bad argument #2 to 'argcheck' (number expected, got ".. type(num) ..")")
+
+	for i=1,select("#", ...) do
+		if type(value) == select(i, ...) then return end
+	end
+
+	local types = {}
+	for i=1, select("#", ...) do types[#types] = select(i, ...) end
+
+	local name = string.match(debug.traceback(2, 2, 0), ": in function ['<](.-)['>]")
+	error(("Bad argument #%d to '%s' (%s expected, got %s"):format(num, name, table.concat(types, ', '), type(value)), 3)
+end
+
+return {
+	__register = {},
+
+	Register = function(self, eventName, eventFunc)
+		argcheck(eventName, 1, "string")
+		argcheck(eventFunc, 2, "function")
+
+		if(not self.__register[eventName]) then self.__register[eventName] = {} end
+
+		local funcs = self.__register[eventName]
+		for i=1, #funcs do
+			if(funcs[i] == func) then
+				return nil, "Event handler already registered."
+			end
+		end
+
+		funcs[#funcs + 1] = eventFunc
+	end,
+
+	Fire = function(self, eventName, ...)
+		argcheck(eventName, 1, "string")
+
+		local funcs = self.__register[eventName]
+		if(funcs) then
+			for i=1, #funcs do
+				funcs[i](...)
+			end
+		end
+	end,
+}
