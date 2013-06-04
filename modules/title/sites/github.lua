e33b42d626bf3429be7467267d5b7a077d73c916
Tor Hveem <tor@hveem.no>
2013-06-04 22:44:23 +0000

    title: add github repo support

--- modules/title/sites/github.lua
+++ modules/title/sites/github.lua
@@ -1,38 +1,36 @@
 local simplehttp = require'simplehttp'
 local json = require'json'
 
-customHosts['gist%.github%.com'] = function(queue, info)
+customHosts['github%.com'] = function(queue, info)
 	local query = info.query
 	local path = info.path
 	local fragment = info.fragment
-	local gid
+    local repo
 
-	local pattern = '/%a+/(%w+)'
+	local pattern = '/(%w+/.+)'
 	if(path and path:match(pattern)) then
-		gid = path:match(pattern)
+	    repo = path:match(pattern)
 	end
 
-	if(gid) then
+	if repo then
 		simplehttp(
-			('https://api.github.com/gists/%s'):format(gid),
+			('https://api.github.com/repos/%s'):format(repo),
 
 			function(data)
 				local info = json.decode(data)
-				local name = info.user
-                if name == json.util.null then 
-                    name = 'Anonymous' 
-                else
-                    name = info.user.login
-                end
-				local files = ''
-                for file,_ in pairs(info.files) do
-                    files = files..file..' ' 
-                end
+				local name = info.name
+                local owner = info.owner.login
+                local description = info.description
 
-                local time = info.updated_at
+                local watchers = info.watchers_count
+                local forks = info.forks_count
+                local lang = info.language
+                if watchers == json.util.null then watchers = 0 end
+                if forks == json.util.null then forks = 0 end
+                if lang == json.util.null then lang = 'Unknown' end
 
 				local out = {}
-                table.insert(out, string.format('\002@%s\002 %s %s', name, time, files))
+                table.insert(out, string.format('\002@%s/%s\002 %s Lang: %s %s watchers %s followers', owner, name, description, lang, watchers, forks))
 
 				queue:done(table.concat(out, ' '))
 			end
