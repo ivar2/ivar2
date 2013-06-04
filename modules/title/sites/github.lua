cb9d6b588466dc39c8567802a5cc8bc6fab70f37
Tor Hveem <tor@hveem.no>
2013-06-04 22:14:34 +0000

    github gist support for now

---
+++ modules/title/sites/github.lua
@@ -0,0 +1,43 @@
+local simplehttp = require'simplehttp'
+local json = require'json'
+
+customHosts['gist%.github%.com'] = function(queue, info)
+	local query = info.query
+	local path = info.path
+	local fragment = info.fragment
+	local gid
+
+	local pattern = '/%a+/(%w+)'
+	if(path and path:match(pattern)) then
+		gid = path:match(pattern)
+	end
+
+	if(gid) then
+		simplehttp(
+			('https://api.github.com/gists/%s'):format(gid),
+
+			function(data)
+				local info = json.decode(data)
+				local name = info.user
+                if name == json.util.null then 
+                    name = 'Anonymous' 
+                else
+                    name = info.user.login
+                end
+				local files = ''
+                for file,_ in pairs(info.files) do
+                    files = files..file..' ' 
+                end
+
+                local time = info.updated_at
+
+				local out = {}
+                table.insert(out, string.format('\002@%s\002 %s %s', name, time, files))
+
+				queue:done(table.concat(out, ' '))
+			end
+		)
+
+		return true
+	end
+end
