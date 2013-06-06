d48b4e6b606d2d022664e520610d99aa13b13394
Trond A Ekseth <troeks@gmail.com>
2013-06-06 20:36:02 +0200

    title: Re-indent github.

--- modules/title/sites/github.lua
+++ modules/title/sites/github.lua
@@ -5,11 +5,11 @@ customHosts['github%.com'] = function(queue, info)
 	local query = info.query
 	local path = info.path
 	local fragment = info.fragment
-    local repo
+	local repo
 
 	local pattern = '/(%w+/[%w-.]+)'
 	if(path and path:match(pattern)) then
-	    repo = path:match(pattern)
+		repo = path:match(pattern)
 	end
 
 	if repo then
@@ -19,18 +19,18 @@ customHosts['github%.com'] = function(queue, info)
 			function(data)
 				local info = json.decode(data)
 				local name = info.name
-                local owner = info.owner.login
-                local description = info.description
+				local owner = info.owner.login
+				local description = info.description
 
-                local watchers = info.watchers_count
-                local forks = info.forks_count
-                local lang = info.language
-                if watchers == json.util.null then watchers = 0 end
-                if forks == json.util.null then forks = 0 end
-                if lang == json.util.null then lang = 'Unknown' end
+				local watchers = info.watchers_count
+				local forks = info.forks_count
+				local lang = info.language
+				if watchers == json.util.null then watchers = 0 end
+				if forks == json.util.null then forks = 0 end
+				if lang == json.util.null then lang = 'Unknown' end
 
 				local out = {}
-                table.insert(out, string.format('\002@%s/%s\002 %s Lang: %s %s watchers %s followers', owner, name, description, lang, watchers, forks))
+				table.insert(out, string.format('\002@%s/%s\002 %s Lang: %s %s watchers %s followers', owner, name, description, lang, watchers, forks))
 
 				queue:done(table.concat(out, ' '))
 			end
