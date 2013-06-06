7a0f2851a6d014ee94de26f2d316196b8520c6c7
Simen Graaten <simeng@irc.lart.no>
2013-06-06 10:54:49 +0000

    Added bitcoin to euro currency module

---
+++ modules/btc.lua
@@ -0,0 +1,18 @@
+local simplehttp = require'simplehttp'
+local json = require'json'
+
+local APIBase = 'https://blockchain.info/no/ticker'
+return {
+	PRIVMSG = {
+		['^.btc$'] = function(self, source, destination, input)
+			simplehttp(APIBase, function(data)
+					local result = parseJSON(data)
+
+					if(result) then
+						self:Msg('privmsg', destination, source, string.format('1BTC is worth %s€ (~15m)', source.nick, result.EUR.15min))
+					end
+				end
+			)
+		end,
+	},
+}
