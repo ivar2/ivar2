67ba7c3c1790cabee9c602a9b4f7670469b5e916
Tor Hveem <tor@hveem.no>
2013-06-20 11:31:51 +0200

    handle two way lookup
    Former-commit-id: 18140f5b26887932c6f523daf54f236e74725d0c [formerly 998988ad45093ff53a4ef300cec25aa521dad78c]
    Former-commit-id: 523598bfa7cb73309c69d1c02cd0e4aa8d8cb760

--- modules/unicode.lua
+++ modules/unicode.lua
@@ -1,10 +1,17 @@
 local sql = require'lsqlite3'
+-- utf-8 functions (C) Rici Lake
+-- http://luaparse.luaforge.net/libquery.lua.html
 local function X(str) return tonumber(str, 16) end
 local elevenBits = X"7FF"
 local sixteenBits = X"FFFF"
 local math = require 'math'
 local mod = math.mod
 local strchar = string.char
+local strbyte = string.byte
+local strfind = string.find
+local offset2 = X"C0" * 64 + X"80"
+local offset3 = X"E0" * 4096 + X"80" * (64 + 1)
+local offset4 = X"F0" * 262144 + X"80" * (4096 + 64 + 1)
 
 
 local function toUtf8(i)
@@ -24,13 +31,30 @@ local function toUtf8(i)
     end
 end
 
+local function fromUtf8(str)
+	if strfind(str, "^[\1-\127%z]$") then return strbyte(str)
+	elseif strfind(str, "^[\194-\223][\128-\191]$") then
+		return strbyte(str, 1) * 64 + strbyte(str, 2) - offset2
+	elseif strfind(str, "^[\225-\236\238\239][\128-\191][\128-\191]$")
+		or strfind(str, "^\224[\160-\191][\128-\191]$")
+		or strfind(str, "^\237[\128-\159][\128-\191]$") then
+		return strbyte(str, 1) * 4096 + strbyte(str, 2) * 64 + strbyte(str, 3)
+		- offset3
+	elseif strfind(str, "^\240[\144-\191][\128-\191][\128-\191]$")
+		or strfind(str, "^[\241\242\243][\128-\191][\128-\191][\128-\191]$")
+		or strfind(str, "^\244[\128-\143][\128-\191][\128-\191]$") then
+		return (strbyte(str, 1) * 262144 - offset4)
+		+ strbyte(str, 2) * 4096 + strbyte(str, 3) * 64 + strbyte(str, 4)
+	end
+end
+
+
 local function handleSearch(self, source, destination, name)
 	local db = sql.open("cache/unicode.sql")
 	local selectStmt  = db:prepare('SELECT * FROM unicode WHERE LOWER(name) LIKE LOWER(?)')
 	selectStmt:bind_values('%'..name..'%')
 
     local out = {}
-    -- self:Msg('privmsg', destination, source, 'hmm:%s', row)
     for row in selectStmt:nrows() do
         table.insert(out, string.format('%s %s', toUtf8(row.cp), row.name))
     end
@@ -42,8 +66,32 @@ local function handleSearch(self, source, destination, name)
 	end
 end
 
+local function handleLookup(self, source, destination, str)
+	local db = sql.open("cache/unicode.sql")
+    local out = {}
+	for uchar in string.gfind(str, "([%z\1-\127\194-\244][\128-\191]*)") do
+		uchar = fromUtf8(uchar)
+		if uchar then 
+			local cp = string.format('%04x', uchar)
+			local selectStmt  = db:prepare('SELECT * FROM unicode WHERE LOWER(cp) LIKE LOWER(?)')
+			selectStmt:bind_values(cp)
+
+			for row in selectStmt:nrows() do
+				table.insert(out, string.format('U+%s %s', (row.cp), row.name))
+			end
+		end
+	end
+
+	db:close()
+
+	if(#out) then
+		self:Msg('privmsg', destination, source, table.concat(out, ', '))
+	end
+end
+
 return {
 	PRIVMSG = {
 		['^%pu (.*)$'] = handleSearch,
+		['^%pw (.*)$'] = handleLookup,
 	}
 }
