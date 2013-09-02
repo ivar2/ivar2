f852b9abe210be671d6b0735f2f01a8d6f047b6d
Trond A Ekseth <troeks@gmail.com>
2013-09-02 23:22:36 +0200

    olds: Switch to SQLite.
    Former-commit-id: c3669dc7af5e7c6054ea0b94e90dbf3f8cf3b3d5 [formerly a6248da49e333f9222d3ea393f6ae70da9de8725]
    Former-commit-id: 57bb4295f9a23c5338cd624c714870e503e70ffc

--- modules/olds.lua
+++ modules/olds.lua
@@ -1,8 +1,5 @@
-local os = require 'os'
-local iconv = require'iconv'
-local dbi = require 'DBI'
-require'logging.console'
-local log = logging.console()
+local sql = require'lsqlite3'
+local date = require'date'
 
 local patterns = {
 	-- X://Y url
@@ -14,72 +11,35 @@ local patterns = {
 	"%f[%S](www%.[%w_-%%]+%.%S+)",
 }
 
--- RFC 2396, section 1.6, 2.2, 2.3 and 2.4.1.
-local smartEscape = function(str)
-	local pathOffset = str:match("//[^/]+/()")
-
-	-- No path means nothing to escape.
-	if(not pathOffset) then return str end
-	local prePath = str:sub(1, pathOffset - 1)
-
-	-- lowalpha: a-z | upalpha: A-Z | digit: 0-9 | mark: -_.!~*'() |
-	-- reserved: ;/?:@&=+$, | delims: <>#%" | unwise: {}|\^[]` | space: <20>
-	local pattern = '[^a-zA-Z0-9%-_%.!~%*\'%(%);/%?:@&=%+%$,<>#%%"{}|\\%^%[%] ]'
-	local path = str:sub(pathOffset):gsub(pattern, function(c)
-		return ('%%%02X'):format(c:byte())
-	end)
-
-	return prePath .. path
-end
-
 -- check for existing url
-local checkOlds = function(self, dbh, destination, source, url)
+local checkOlds = function(self, destination, source, url)
+	local db = sql.open("cache/urls.sql")
 	-- create a select handle
-	local sth = assert(dbh:prepare([[
+	local sth = db:prepare([[
 		SELECT
-			date_trunc('second', time),
-			date_trunc('second', age(now(), date_trunc('second', time))),
-			nick
+			nick,
+			timestamp,
+			count
 		FROM urls
 		WHERE
 			url=?
 			AND
 			channel=?
 		ORDER BY time ASC
-	]]))
+	]])
 
 	-- execute select with a url bound to variable
-	sth:execute(url,destination)
+	sth:bind_values(url, destination)
+	local iter, vm = sth:nrows()
+	local row = iter(vm)
 
-	-- get list of column names in the result set
-	--local columns = sth:columns()
+	if(row and row.count > 0) then
+		local age = date.relativeTimeShort(os.time() - row.timestamp)
 
-	local count = 0
-	local nick
-	local when
-
-	-- iterate over the returned data
-	for row in sth:rows() do
-		count = count + 1
-		-- rows() with no arguments (or false) returns
-		-- the data as a numerically indexed table
-		-- passing it true returns a table indexed by
-		-- column names
-		if count == 1 then
-			when = row[1]
-			ago = row[2]
-			nick = row[3]
-			when = when .. ', ' .. ago .. ' ago'
-		end
-	end
-
-	if count > 0 then
-		local plural = ''
-		if count > 1 then
-			plural = 's'
-			ivar2:Msg('privmsg', destination, source, 'Olds! Linked %s time%s before. First %s by %s', count, plural, when, nick)
+		if(count > 1) then
+			ivar2:Msg('privmsg', destination, source, 'Old! Linked %s times before. First %s by %s', count, age, row.nick)
 		else
-			ivar2:Msg('privmsg', destination, source, 'Olds! Linked before at %s by %s', when, nick)
+			ivar2:Msg('privmsg', destination, source, 'Old! Linked before, %s by %s', age, row.nick)
 		end
 	end
 end
@@ -87,15 +47,7 @@ end
 local handleUrl = function(self, source, destination, msg, url)
 	-- Check if this module is disabled and just stop here if it is
 	if not self:IsModuleDisabled('olds', destination) then
-		local nick = source.nick
-
-		-- TODO save connection
-		local dbh = DBI.Connect('PostgreSQL', self.config.dbname, self.config.dbuser, self.config.dbpass, self.config.dbhost, self.config.dbport)
-
-		checkOlds(self, dbh, destination, source, url)
-
-		--local ok = dbh:close()
-		--
+		checkOlds(self, destination, source, url)
 	end
 
 	-- Fire the oldsdone event for sqllogger
