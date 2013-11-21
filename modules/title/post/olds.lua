7a1b622c8e9fc2ac37da28f3e231b8f23d4889fd
Trond A Ekseth <troeks@gmail.com>
2013-11-21 21:32:26 +0100

    title/olds: Remove unused function.
    Former-commit-id: 64debf9b4e209345ef2cc146979087508ecfb902 [formerly 904b7fc143dfcbbca857922dfd44427d68ee8298]
    Former-commit-id: 64349239ff32daf79dc7539aa62fcf40899886ab

--- modules/title/post/olds.lua
+++ modules/title/post/olds.lua
@@ -27,24 +27,6 @@ local openDB = function()
 	return db
 end
 
--- RFC 2396, section 1.6, 2.2, 2.3 and 2.4.1.
-local smartEscape = function(str)
-	local pathOffset = str:match("//[^/]+/()")
-
-	-- No path means nothing to escape.
-	if(not pathOffset) then return str end
-	local prePath = str:sub(1, pathOffset - 1)
-
-	-- lowalpha: a-z | upalpha: A-Z | digit: 0-9 | mark: -_.!~*'() |
-	-- reserved: ;/?:@&=+$, | delims: <>#%" | unwise: {}|\^[]` | space: <20>
-	local pattern = '[^a-zA-Z0-9%-_%.!~%*\'%(%);/%?:@&=%+%$,<>#%%"{}|\\%^%[%] ]'
-	local path = str:sub(pathOffset):gsub(pattern, function(c)
-		return ('%%%02X'):format(c:byte())
-	end)
-
-	return prePath .. path
-end
-
 -- check for existing url
 local checkOld = function(source, destination, url)
 	local db = openDB()
