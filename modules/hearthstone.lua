7c26bdf7ab90e9b81a23fbdbf9484b1e153913e8
Trond A Ekseth <troeks@gmail.com>
2013-12-08 20:44:30 +0100

    hearthstone: New module
    Former-commit-id: 9a7a3983b891e354626e548b138a84ca7208b9ca [formerly b0a65711f8d3d554638bd855f43c0dd30b8f1ed0]
    Former-commit-id: 1605be26e8c867732bbc99a6241be3af24d4fbdc

---
+++ modules/hearthstone.lua
@@ -0,0 +1,121 @@
+local simplehttp = require'simplehttp'
+
+local cache = {}
+local trim = function(s)
+	return s:match('^%s*(.-)%s*$')
+end
+
+local pattern = ('<td[^>]*>([^\n]+)\n[^<]+'):rep(10)
+local parseData = function(data)
+	local tmp = {}
+	local tbody = data:match('<tbody>(.*)</tbody>')
+	for row in tbody:gmatch('<tr[^>]+>.-</tr>') do
+		for _, name, class, rarity, kind, race, mana, attack, life, desc in row:gmatch(pattern) do
+			-- Strip HTML:
+			name = name:gsub('<%/?[%w:]+.-%/?>', '')
+			class = class:match('(%w)%.png"') or class
+
+			tmp[name:lower()] = {
+				name = name,
+				class = class:sub(1, -6),
+				rarity = rarity:sub(1, -6),
+				type = kind:sub(1, -6),
+				race = race ~= "" and race:sub(1, -6) or nil,
+				mana = mana ~= "" and mana:sub(1, -6) or nil,
+				attack = attack ~= "" and attack:sub(1, -6) or nil,
+				life = life ~= "" and attack:sub(1, -6) or nil,
+				desc = desc ~= "" and desc:sub(1, -6) or nil,
+			}
+		end
+	end
+
+	if(next(tmp)) then
+		cache = tmp
+	end
+end
+
+local formatOutput = function(card)
+	local out = {}
+
+	card = cache[card]
+	table.insert(out, card.name)
+	table.insert(out, card.class)
+	table.insert(out, card.rarity)
+	table.insert(out, card.type)
+
+	if(card.mana) then
+		table.insert(out, string.format('Cost: %s', card.mana))
+	end
+
+	if(card.attack) then
+		table.insert(out, string.format('Attack: %s', card.mana))
+	end
+
+	if(card.life) then
+		table.insert(out, string.format('HP: %s', card.mana))
+	end
+
+	if(card.desc) then
+		table.insert(out, card.desc)
+	end
+
+	return table.concat(out, ' / ')
+end
+
+local checkCache = function(card)
+	local out
+	if(cache[card]) then
+		out = formatOutput(card)
+	end
+
+	if(not out) then
+		local matches = {}
+		for name, data in next, cache do
+			if(name:find(card, 1, true)) then
+				table.insert(matches, data.name)
+			end
+		end
+
+		if(#matches == 1) then
+			out = formatOutput(matches[1]:lower())
+		elseif(#matches > 1) then
+			local n = 7
+			out = {}
+			local msgLimit = (512 - 16 - 65 - 10) - #ivar2.config.nick - 30
+			for i=1, #matches do
+				local name = matches[i]
+				n = n + #name + 2
+				if(n < msgLimit) then
+					table.insert(out, string.format('%s', name))
+				end
+			end
+
+			out = 'Found: ' .. table.concat(out, ', ')
+		end
+	end
+
+	return out
+end
+
+return {
+	PRIVMSG = {
+		['^!hs (.+)$'] = function(self, source, destination, card)
+			card = trim(card:lower())
+
+			local out = checkCache(card)
+			if(not out) then
+				simplehttp('http://hearthstonecardlist.com/', function(data)
+					-- Update cache.
+					parseData(data)
+
+					local out = checkCache(card)
+					if(out) then
+						self:Msg('privmsg', destination, source, 'Hearthstone: %s', out)
+					else
+						self:Msg('privmsg', destination, source, 'Hearthstone: No matching card found.')
+					end
+				end)
+			end
+		end,
+	},
+}
