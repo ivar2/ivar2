2c7df0927331e0d695e774e78bfb1e57b863940a
Tor Hveem <tor@hveem.no>
2014-04-26 11:46:10 +0000

    title/github: retab

--- modules/title/sites/github.lua
+++ modules/title/sites/github.lua
@@ -2,39 +2,39 @@ local simplehttp = require'simplehttp'
 local json = require'json'
 
 customHosts['^github%.com'] = function(queue, info)
-	local query = info.query
-	local path = info.path
-	local fragment = info.fragment
-	local repo
+    local query = info.query
+    local path = info.path
+    local fragment = info.fragment
+    local repo
 
-	local pattern = '/([%w-.]+/[%w-.]+)/?$'
-	if(path and path:match(pattern)) then
-		repo = path:match(pattern)
-	end
+    local pattern = '/([%w-.]+/[%w-.]+)/?$'
+    if(path and path:match(pattern)) then
+        repo = path:match(pattern)
+    end
 
-	if repo then
-		simplehttp(
-			('https://api.github.com/repos/%s'):format(repo),
+    if repo then
+        simplehttp(
+            ('https://api.github.com/repos/%s'):format(repo),
 
-			function(data)
-				local info = json.decode(data)
-				local name = info.name
-				local owner = info.owner.login
-				local description = info.description
+            function(data)
+                local info = json.decode(data)
+                local name = info.name
+                local owner = info.owner.login
+                local description = info.description
 
-				local watchers = info.watchers_count
-				local forks = info.forks_count
-				local lang = info.language
+                local watchers = info.watchers_count
+                local forks = info.forks_count
+                local lang = info.language
                 local issues = info.open_issues_count
-				if watchers == json.util.null then watchers = 0 end
-				if forks == json.util.null then forks = 0 end
-				if lang == json.util.null then lang = 'Unknown' end
+                if watchers == json.util.null then watchers = 0 end
+                if forks == json.util.null then forks = 0 end
+                if lang == json.util.null then lang = 'Unknown' end
                 if issues == json.util.null then issues = 0 end
 
-				queue:done(string.format('\002@%s/%s\002 %s, Lang: \002%s\002, \002%s\002 watchers, \002%s\002 forks, \002%s\002 open issues', owner, name, description, lang, watchers, forks, issues))
-			end
-		)
+                queue:done(string.format('\002@%s/%s\002 %s, Lang: \002%s\002, \002%s\002 watchers, \002%s\002 forks, \002%s\002 open issues', owner, name, description, lang, watchers, forks, issues))
+            end
+        )
 
-		return true
-	end
+        return true
+    end
 end
