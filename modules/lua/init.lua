acb949114436a7f9f0fb007fc83e8a4752bebd6b
Tor Hveem <tor@hveem.no>
2014-05-07 20:42:32 +0000

    lua: don't truncate output
    Former-commit-id: 334d6dcd9e3af8593c9656cbd86d3b205821c6a4 [formerly 4e000954ddbde297f07a1183f64eaa9183c80522]
    Former-commit-id: 537a372f8f4b895346c35d17a5a1f6b53984dca5

--- modules/lua/init.lua
+++ modules/lua/init.lua
@@ -16,7 +16,7 @@ return {
 			local cmd = string.format([[modules/lua/sandbox.sh %s %s 2> /dev/null]], inputFile, outputFile)
 			local status = os.execute(cmd) / 256
 			local file = io.open(outputFile, 'r')
-			local output = file:read(251)
+			local output = file:read(1024)
 			file:close()
 
 			-- Clean up our mess
@@ -39,8 +39,6 @@ return {
 
 			if(not output:match('%S')) then
 				output = 'No output'
-			elseif(#output > 250) then
-				output = output:sub(1, 250) .. '(truncated)'
 			end
 
 			ivar2:Msg('privmsg', destination, source, '%s: %s', source.nick, output)
