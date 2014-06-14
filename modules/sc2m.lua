44304d391449d9f6575413bb5003a590a9306346
Trond A Ekseth <troeks@gmail.com>
2014-06-14 15:13:26 +0200

    sc2m: Fix pattern.
    Former-commit-id: 08a60445e01869157049a5c0598103698a4ef8b5 [formerly 7bcf411679fd440dfd35a4141d6ad7bf26110fa3]
    Former-commit-id: 986cb4c9bc275fb88e454c11f6576c720ad68bfa

--- modules/sc2m.lua
+++ modules/sc2m.lua
@@ -1,6 +1,6 @@
 return {
 	PRIVMSG = {
-		['^!sc2m (%d+%.?%d*) (%d+%.?%d*)$'] = function(self, source, destination, a, b)
+		['^%psc2m (%d+%.?%d*) (%d+%.?%d*)$'] = function(self, source, destination, a, b)
 			self:Msg('privmsg', destination, source, '%s: %s', source.nick, ((a + 1)*10)^(1 + math.log10(b + 1)))
 		end
 	}
