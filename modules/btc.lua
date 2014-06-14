46e1f73c993b3b036a2ca0ce0f04c6f3c674aa34
Trond A Ekseth <troeks@gmail.com>
2014-06-14 15:13:26 +0200

    btc: Fix pattern.
    Former-commit-id: 5a9a15130e19660b167b4e60b4c519b257713fc9 [formerly 31955254cb74be03a670dc784e869df485c4b004]
    Former-commit-id: ace07a854741ec55b417404f57eb90af37eef3ba

---
+++ modules/btc.lua
@@ -0,0 +1,18 @@
+local simplehttp = require'simplehttp'
+local json = require'json'
+
+local APIBase = 'https://blockchain.info/no/ticker'
+return {
+	PRIVMSG = {
+		['^%pbtc$'] = function(self, source, destination, input)
+			simplehttp(APIBase, function(data)
+					local result = json.decode(data)
+
+					if(result) then
+						self:Msg('privmsg', destination, source, '\0021\002 BTC is worth \002%s\002 € (~15m)', result["EUR"]["15m"])
+					end
+				end
+			)
+		end,
+	},
+}
