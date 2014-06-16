de7e785f2c406d49f6908f5f531c955060428057
Tor Hveem <tor@hveem.no>
2014-06-16 22:39:10 +0000

    rottentomatoes: use new util lib. Colorize score

--- modules/rottentomatoes.lua
+++ modules/rottentomatoes.lua
@@ -1,38 +1,13 @@
-local simplehttp = require'simplehttp'
-local json = require'json'
-
-local utify8 = function(str)
-	str = str:gsub("\\u(....)", function(n)
-		n = tonumber(n, 16)
-
-		if(n < 128) then
-			return string.char(n)
-		elseif(n < 2048) then
-			return string.char(192 + ((n - (n % 64)) / 64), 128 + (n % 64))
-		else
-			return string.char(224 + ((n - (n % 4096)) / 4096), 128 + (((n % 4096) - (n % 64)) / 64), 128 + (n % 64))
-		end
-	end)
-
-	return str
-end
-
-local urlEncode = function(str)
-	return str:gsub(
-		'([^%w ])',
-		function (c)
-			return string.format ("%%%02X", string.byte(c))
-		end
-	):gsub(' ', '+')
-end
-
-local trim = function(s)
-	return s:match('^%s*(.-)%s*$')
+local colorize = function(score)
+    if tonumber(score) > 50 then
+        return ivar2.util.red(string.format("%s%%", score))
+    else
+        return ivar2.util.green(string.format("%s%%", score))
+    end
 end
 
 local parseData = function(source, destination, data, search)
-	data = utify8(data)
-	data = json.decode(data)
+	data = ivar2.util.json.decode(data)
 
 	local found
 	for i=1, #data.movies do
@@ -65,8 +40,8 @@ local parseData = function(source, destination, data, search)
 	end
 
 	ins(
-		"\002%s\002 (%d)",
-		movie['title'], movie['year'], movie['mpaa_rating'], movie['runtime']
+		"%s (%d)",
+		ivar2.util.bold(movie['title']), movie['year'], movie['mpaa_rating'], movie['runtime']
 	)
 
 	if(movie['runtime'] ~= "") then
@@ -75,10 +50,10 @@ local parseData = function(source, destination, data, search)
 		ins("%s", movie['mpaa_rating'])
 	end
 
-	ins("- Critics: \002%d\002%%", movie['ratings']['critics_score'])
+	ins("- Critics: %s", colorize(movie['ratings']['critics_score']))
 	ins("(%s)", movie['ratings']['critics_rating'])
 
-	ins("- Audience: \002%d\002%%", movie['ratings']['audience_score'])
+	ins("- Audience: %s", colorize(movie['ratings']['audience_score']))
 	ins("(%s)", movie['ratings']['audience_rating'])
 
 	ins("- %s", movie['critics_consensus'])
@@ -88,9 +63,9 @@ end
 
 local urlFormat = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?page_limit=5&apikey=%s&q=%s'
 local handler = function(self, source, destination, input)
-	local search = urlEncode(trim(input))
+	local search = ivar2.util.urlEncode(ivar2.util.trim(input))
 
-	simplehttp(
+	ivar2.util.simplehttp(
 		urlFormat:format(self.config.rottenTomatoesAPIKey, search),
 		function(data)
 			say(parseData(source, destination, data, input:lower()))
