9207d8eb889c78ec962748850a41e12764935148
Trond A Ekseth <troeks@gmail.com>
2014-12-13 13:40:43 +0100

    title/github: Switch to the util library.

--- modules/title/sites/github.lua
+++ modules/title/sites/github.lua
@@ -1,5 +1,6 @@
-local simplehttp = require'simplehttp'
-local json = require'json'
+local util = require'util'
+local simplehttp = util.simplehttp
+local json = util.json
 
 customHosts['^github%.com'] = function(queue, info)
     local query = info.query
