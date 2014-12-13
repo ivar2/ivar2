3a8951537ed562774ba54528ce4d51a111cb5f69
Trond A Ekseth <troeks@gmail.com>
2014-12-13 13:40:43 +0100

    title: Switch to the util library.
    Former-commit-id: 0a9fa8480da54a77f70c9c80ad521f11c5988cc2 [formerly 8817a60858ea58f00aa8f94bf13c3b4ccf4706cc]
    Former-commit-id: faf931706e36b93f85be35db7ec1d7f2a9f726f5

--- modules/title/init.lua
+++ modules/title/init.lua
@@ -1,7 +1,8 @@
+local util = require'util'
+local simplehttp = util.simplehttp
+local trim = util.trim
 local iconv = require"iconv"
 local uri = require"handler.uri"
-
-local simplehttp = require'simplehttp'
 local x0 = require'x0'
 local html2unicode = require'html'
 local nixio = require'nixio'
@@ -25,10 +26,6 @@ local translateCharset = {
 	['ks_c_5601-1987'] = 'euc-kr',
 	['ksc_5601'] = 'euc-kr',
 }
-
-local trim = function(str)
-	return str:match('^%s*(.-)%s*$')
-end
 
 -- RFC 2396, section 1.6, 2.2, 2.3 and 2.4.1.
 local smartEscape = function(str)
