fc7e1880862ec7c4aad1f98b0a4a02089cf48422
Trond A Ekseth <troeks@gmail.com>
2014-12-13 13:45:22 +0100

    shipment: Remove trailing whitespaces and fix mixed indent.
    Conflicts:
    	modules/shipment.lua
    
    Former-commit-id: b3ba0d8c34cd2e0c19ab409bc2f20adfbc2fef3b [formerly ec8d6bfccede82a9af3b95a4284105ba034e1475]
    Former-commit-id: e28001175cbad2b0eac73d51c17d587401c1b05b

--- modules/shipment.lua
+++ modules/shipment.lua
@@ -46,21 +46,21 @@ local shipmentTrack = function(self, source, destination, message)
 
 	-- Store the eventcount in the ivar2 global
 	-- if the eventcount increases it means new events on the shipment happened.
-	local eventCount = self.shipmentEvents[id] 
-	if not eventCount then 
+	local eventCount = self.shipmentEvents[id]
+	if not eventCount then
 		self.shipmentEvents[id] = -1
 	end
 
 	self:Timer('shipment', duration,
 		function(loop, timer, revents)
-			simplehttp(string.format(apiurl, pid, getCacheBust()), function(data) 
+			simplehttp(string.format(apiurl, pid, getCacheBust()), function(data)
 				local info = json.decode(data)
 				local cs = info.consignmentSet
 				if not cs[1] then return else cs = cs[1] end
 				local err = cs['error']
 				if err then
 					local errmsg = err['message']
-					if self.shipmentEvents[id] == -1 then 
+					if self.shipmentEvents[id] == -1 then
 						say('%s: \002%s\002 %s', nick, alias, errmsg)
 					end
 					self.shipmentEvents[id] = 0
@@ -71,8 +71,8 @@ local shipmentTrack = function(self, source, destination, message)
 				local newEventCount = #eventset
 				local out = {}
 				--print('id:',id,'new:',newEventCount,'old:',self.shipmentEvents[id])
-				if newEventCount < self.shipmentEvents[id] 
-					then newEventCount = self.shipmentEvents[id] 
+				if newEventCount < self.shipmentEvents[id]
+					then newEventCount = self.shipmentEvents[id]
 				end
 				for i=self.shipmentEvents[id]+1,newEventCount do
 					--print('loop:',i)
@@ -97,7 +97,7 @@ end
 
 local shipmentLocate = function(self, source, destination, pid)
 	local nick = source.nick
-	simplehttp(string.format(apiurl, pid, getCacheBust()), function(data) 
+	simplehttp(string.format(apiurl, pid, getCacheBust()), function(data)
 		local info = json.decode(data)
 		local cs = info.consignmentSet
 		if not cs[1] then return else cs = cs[1] end
