38754b05365564d28d32a26a24a86aa9021b9bda
Trond A Ekseth <troeks@gmail.com>
2014-12-13 13:43:39 +0100

    dokpro: Remove trailing whitespaces.
    Former-commit-id: 7a3993e348fd5fc3d908a69ad36521b16d6d0ef7 [formerly 060e95793156396bbf7d8f42e223c1a148321121]
    Former-commit-id: e517ea218b1ed5a951e90351323c20fe600fdce2

--- modules/dokpro.lua
+++ modules/dokpro.lua
@@ -26,13 +26,13 @@ local parseData = function(data)
 			word = doc[1][1][1]
 		end
 		-- First entry
-		local entry = { 
+		local entry = {
 			lookup = {},
 			meaning = {},
 			examples = {},
 		}
 		local addentry = function(lookup)
-			entry = { 
+			entry = {
 				lookup = {},
 				meaning = {},
 				examples = {},
@@ -46,7 +46,7 @@ local parseData = function(data)
 		end
 		-- Here be dragons. This is why we can't have nice things
 		for _, w in pairs(doc) do
-			if _ ~= '_tag' then 
+			if _ ~= '_tag' then
 				if type(w) == type("") then
 					add(w)
 				elseif type(w) == type({}) then
@@ -142,7 +142,7 @@ return {
 		['^%pdokpro (.+)$'] = handleInput,
 		['^%pordbok (.+)$'] = handleInput,
 		['^%pbokmål (.+)$'] = handleInput,
-		['^%pnynorsk (.+)$'] = function(self, source, destination, word) 
+		['^%pnynorsk (.+)$'] = function(self, source, destination, word)
 			handleInput(self, source, destination, word, 'nynorsk')
 		end
 	},
