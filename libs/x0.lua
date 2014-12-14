31657c3d3865589b78e25d4326a57bed31c668b5
Trond A Ekseth <troeks@gmail.com>
2014-12-14 17:56:13 +0100

    x0: Switch to ivar2.persist.
    Former-commit-id: b2fb752a190c64acdaaee0a08b4e8adc10c06de6 [formerly d2887387c97dd59c724e1135763a839377629e60]
    Former-commit-id: 27e7d51e547a56a2e687be50097415daa78eb351

--- libs/x0.lua
+++ libs/x0.lua
@@ -1,28 +1,19 @@
 local simplehttp = require'simplehttp'
-require'tokyocabinet'
-
-local x0 = tokyocabinet.hdbnew()
+local x0 = ivar2.persist
 
 return {
 	lookup = function(url, callback)
-		x0:open('cache/x0', x0.OWRITER + x0.OCREAT)
-		if(x0[url]) then
-			local url = x0[url]
-			x0:close()
+		if(x0["x0:" .. url]) then
+			local url = x0["x0:" .. url]
 
 			return callback(url)
-		else
-			x0:close()
 		end
-
 
 		simplehttp(
 			'http://api.x0.no/?' .. url,
 			function(data, url)
 				if(data:sub(8,9) =='x0') then
-					x0:open('cache/x0', x0.OWRITER + x0.OCREAT)
-					x0[url] = data
-					x0:close()
+					x0["x0:" .. url] = data
 
 					return callback(data)
 				end
