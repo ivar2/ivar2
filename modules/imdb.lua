c9ffe9bf9001e2078d7389408d161e452729553e
Trond A Ekseth <troeks@gmail.com>
2014-12-15 19:35:56 +0100

    imdb: utify8 is no longer required.
    Former-commit-id: 3a51c9a18cc2ac191827ff23496a05cb31f67a56 [formerly d2cd450e103c68b42111fb5ecd268990dc0d8179]
    Former-commit-id: 7240c45af99abfe5eb3f97903b711d16d794182b

--- modules/imdb.lua
+++ modules/imdb.lua
@@ -3,24 +3,7 @@ local simplehttp = util.simplehttp
 local json = util.json
 local urlEncode = util.urlEncode
 
-local utify8 = function(str)
-	str = str:gsub("\\u(....)", function(n)
-		n = tonumber(n, 16)
-
-		if(n < 128) then
-			return string.char(n)
-		elseif(n < 2048) then
-			return string.char(192 + ((n - (n % 64)) / 64), 128 + (n % 64))
-		else
-			return string.char(224 + ((n - (n % 4096)) / 4096), 128 + (((n % 4096) - (n % 64)) / 64), 128 + (n % 64))
-		end
-	end)
-
-	return str
-end
-
 local parseData = function(source, destination, data)
-	data = utify8(data)
 	data = json.decode(data)
 	local search = data.Search
 
