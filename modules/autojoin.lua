de321cc7c2d0350d4f1a7f72dea10233c252c026
Tor Hveem <tor@hveem.no>
2014-12-28 11:34:58 +0000

    autojoin: use new timer interface, and log interface

--- modules/autojoin.lua
+++ modules/autojoin.lua
@@ -1,6 +1,3 @@
-require'logging.console'
-local log = logging.console()
-
 local join = function(self, channel, data)
 	if(type(data) == 'table' and data.password) then
 		self:Join(channel, data.password)
@@ -14,31 +11,20 @@ return {
 		function(self)
 			if(self.config.channels) then
 				for channel, data in next, self.config.channels do
-					log:info(string.format('Automatically joining %s.', channel))
+					self:Log('info', string.format('Automatically joining %s.', channel))
 					join(self, channel, data)
 				end
 
 
 				local timerName = 'autojoin'
-				if(not self.timers) then self.timers = {} end
-
-				if(not self.timers[timerName]) then
-					local timer = ev.Timer.new(
-						function(loop, timer, revents)
-							for channel, data in next, self.config.channels do
-								if(not self.channels[channel]) then
-									log:info(string.format('Automatically rejoining %s.', channel))
-									join(self, channel, data)
-								end
-							end
-						end,
-						60,
-						60
-					)
-
-					self.timers[timerName] = timer
-					timer:start(self.Loop)
-				end
+				local timer = self:Timer(timerName, 60, 60, function(loop, timer, revents)
+					for channel, data in next, self.config.channels do
+						if(not self.channels[channel]) then
+							self:Log('info', string.format('Automatically rejoining %s.', channel))
+							join(self, channel, data)
+						end
+					end
+				end)
 			end
 		end,
 	}
