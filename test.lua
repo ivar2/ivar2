9a50b23b8f03a714fe09da89ed48a77a5275d609
Tor Hveem <tor@hveem.no>
2015-01-04 10:51:24 +0000

    util: add some utf8 string helpers
    Former-commit-id: ad8b12f5d43c6bf32d7aa0b62d17f50c416b2f68 [formerly 9c52ee2b64ae976b6ab628669857489b79524f72]
    Former-commit-id: ad69584f3f4a870b1b9fedaa69fcf1f2c05f6a8d

--- test.lua
+++ test.lua
@@ -14,11 +14,12 @@ package.cpath = table.concat({
 
 local util = require 'util'
 local irc = require 'irc'
+local utf8 = util.utf8
 
 describe("test IRC parser", function()
     describe("352 message", function()
         it("should parse 352 message with IPv6 host", function()
-              line = ':server.server.com 352 botnick #channel user 2a00:dd52:211g::2 server.server.com nick H :0 Realname'
+              local line = ':server.server.com 352 botnick #channel user 2a00:dd52:211g::2 server.server.com nick H :0 Realname'
               local command, argument, source, destination = irc.parse(line)
               assert.are_equal('352', command)
               assert.are_equal('server.server.com', source)
@@ -31,6 +32,28 @@ describe("test IRC parser", function()
                   realname = 'Realname',
                   user = 'user'
               }, argument)
+        end)
+    end)
+end)
+
+describe("test util lib", function()
+    describe("utf8 string tests", function()
+        it("should work with multibye utf8 chars", function()
+            local line = {'F','o','o',' ','æ','ø','Å','😀'}
+            local uline = {}
+            for c in util.utf8.chars(table.concat(line)) do
+                table.insert(uline, c)
+            end
+            assert.are_same(line, uline)
+            assert.are_equal(#line, utf8.len(table.concat(line)))
+            local reversed = {}
+            for i=#line,1,-1 do
+                table.insert(reversed, line[i])
+            end
+            assert.are_same(table.concat(reversed), utf8.reverse(table.concat(line)))
+
+            assert.are_equal('foo æøå😀', utf8.lower(table.concat(line)))
+
         end)
     end)
 end)
