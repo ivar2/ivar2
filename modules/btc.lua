57aca6bca6ae851fdc181bc1f37fc3f97f7253e9
Trond A Ekseth <troeks@gmail.com>
2015-01-07 19:32:26 +0100

    btc: Minor code cleanup.

--- modules/btc.lua
+++ modules/btc.lua
@@ -3,17 +3,18 @@ local simplehttp = util.simplehttp
 local json = util.json
 
 local APIBase = 'https://blockchain.info/no/ticker'
+local handler = function(self, source, destination, input)
+	simplehttp(APIBase, function(data)
+		local result = json.decode(data)
+
+		if(result) then
+			self:Msg('privmsg', destination, source, '\0021\002 BTC is worth \002%s\002 € (~15m)', result["EUR"]["15m"])
+		end
+	end)
+end
+
 return {
 	PRIVMSG = {
-		['^%pbtc$'] = function(self, source, destination, input)
-			simplehttp(APIBase, function(data)
-					local result = json.decode(data)
-
-					if(result) then
-						self:Msg('privmsg', destination, source, '\0021\002 BTC is worth \002%s\002 € (~15m)', result["EUR"]["15m"])
-					end
-				end
-			)
-		end,
+		['^%pbtc$'] = handler,
 	},
 }
