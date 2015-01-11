52ad6ce92916a99f3fc88af35865d00082188fef
Trond A Ekseth <troeks@gmail.com>
2015-01-11 22:16:10 +0100

    yr: Properly escape searches.

--- modules/yr.lua
+++ modules/yr.lua
@@ -275,7 +275,7 @@ return {
 				return
 			end
 
-			local url = apiBase:format(place)
+			local url = apiBase:format(self.util.urlEncode(place))
 			if(country) then
 				url = url .. '&country=' .. country
 			end
