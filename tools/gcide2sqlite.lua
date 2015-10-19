3e3e010d1e50659562d34f56a6eeed9808f38e22
Tor Hveem <tor@hveem.no>
2015-10-19 16:24:03 +0200

    tools/gcide2sqlite: support multiple defs
    Former-commit-id: 9c416a84409044078300e662534fceda2045d994 [formerly 04b1eee89a7ced13e4abc1a836b4cbdbd8a38edc]
    Former-commit-id: 01f5b8c4d59767166d1a4ed57186c13c9f39aa8d

--- tools/gcide2sqlite.lua
+++ tools/gcide2sqlite.lua
@@ -23,7 +23,7 @@ db:exec"PRAGMA journal_mode=MEMORY"
 db:exec"PRAGMA temp_store=MEMORY"
 db:exec"PRAGMA auto_vacuum=FULL"
 
-local ret, err = db:exec[[
+db:exec[[
   CREATE TABLE IF NOT EXISTS words (
     word text,
     pos text,
@@ -33,38 +33,31 @@ local ret, err = db:exec[[
 ]]
 db:exec'DELETE FROM words;'
 
--- Cache uniques in mem
-local words =  {}
-
 for c in ('abcdefghijklmnopqrstuvxyz'):gmatch('.') do
 
   local file = 'CIDE.'..c:upper()
   local fd = io.open(file)
-  local entry = {}
   while true do
-      line = fd:read('*l')
+      local line = fd:read('*l')
       if not line then break end
       local entry = line:match('<hw>([^<]+)</hw>')
       local pos = line:match('<pos>([^<]+)</pos>')
       local def = line:match('<def>([^<]+)</def>')
       local field = line:match('<fld>([^<]+)</fld>')
       if entry and pos and def then
-          entry, _ = entry:gsub('[^A-Za-z0-9]', '')
-          if not words[entry] then
-              words[entry] = true
-              -- Remove brackets
-              def, _ = def:gsub("%b<>", "")
-              def, _ = def:gsub('[^A-Za-z%-%.; ]', '')
-              db:execute 'BEGIN TRANSACTION'
-              entry = utf2iso:iconv(entry)
-              def= utf2iso:iconv(def)
-              --print(entry, pos, field, def)
-              local stmt = db:prepare('INSERT INTO words (word, pos, field, definition) VALUES (?, ?, ?, ?)')
-              stmt:bind_values(entry, pos, field, def)
-              stmt:step()
-              stmt:finalize()
-              db:execute 'COMMIT TRANSACTION'
-            end
+          entry = (entry:gsub('[^A-Za-z0-9]', ''))
+          -- Remove brackets
+          def = (def:gsub("%b<>", ""))
+          def = (def:gsub('[^A-Za-z%-%.; ]', ''))
+          db:execute 'BEGIN TRANSACTION'
+          entry = utf2iso:iconv(entry)
+          def= utf2iso:iconv(def)
+          --print(entry, pos, field, def)
+          local stmt = db:prepare('INSERT INTO words (word, pos, field, definition) VALUES (?, ?, ?, ?)')
+          stmt:bind_values(entry, pos, field, def)
+          stmt:step()
+          stmt:finalize()
+          db:execute 'COMMIT TRANSACTION'
         end
     end
 end
