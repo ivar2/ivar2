075241c5e7e4ab7936348e47f6f39ed7935dc570
Tor Hveem <tor@hveem.no>
2016-05-29 11:48:25 +0200

    x0: code cleanup, no shadow var
    Former-commit-id: 950a4cad514d3a3e475dd6bc85463f46763ae8a3
    Former-commit-id: 6dd93c3b25adbfc133fb81d05b998863f8897ed9

--- core/x0.lua
+++ core/x0.lua
@@ -4,16 +4,16 @@ local util = require'util'
 return function(url, callback)
 	local x0 = ivar2.persist
 	if(x0["x0:" .. url]) then
-		local url = x0["x0:" .. url]
+		local nurl = x0["x0:" .. url]
 
-		return callback(url)
+		return callback(nurl)
 	end
 
 	util.simplehttp(
 		'https://xt.gg/url?url=' .. util.urlEncode(url),
-		function(data, url)
+		function(data, realurl)
 			data = util.json.decode(data).url
-			x0["x0:" .. url] = data
+			x0["x0:" .. realurl] = data
 			return callback(data)
 		end
 	)
