b7dc254245fec73d3e7aecda6e503175d3773486
Tor Hveem <tor@hveem.no>
2017-09-01 11:17:29 +0200

    title: shorten wikipedia summaries

--- modules/title/sites/wikipedia.lua
+++ modules/title/sites/wikipedia.lua
@@ -17,10 +17,14 @@ customHosts['wikipedia.org'] = function(queue, info)
 				if not entry or not entry.extract then
 				queue:done('Missing entry')
 				else
-					-- Sometimes text is very long, let's try to snip some
+					-- Sometimes text is very long, let's try to snip some by
+					-- using only first sentence
 					local text = entry.extract
-					text = text:match('(.-)\n') or text
-					queue:done(text)
+					local short_text = text:match('(.-)%.') or text
+					-- If first sentence is very short, let's get some more
+					--if #short_text < 30
+					--short_text = text:match('(.-)\n') or text
+					queue:done(short_text .. '.')
 				end
 			end
 		)
